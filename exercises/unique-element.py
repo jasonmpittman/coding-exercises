5e215b81ec2f203b5ed76dbe68a5f1f119e977c2
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-08 05:12:24 -0500

    initial commit

---
+++ exercises/unique-element.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Unique Element:
+
+Write a function that finds the element that appears exactly once in an array where every other element appears exactly twice. The array will contain integers, and you can assume that there is always exactly one element that appears only once.
+
+    findUniqueElement([4, 3, 2, 4, 3]) should return 2.
+    findUniqueElement([7, 3, 5, 5, 4, 3, 4]) should return 7.
+
+    43243
+    
+Started: Dec 08, 2023 @ 5:10am ET
+Intervals: 1
+Ended: Dec 08, 2023 @ 5:40am ET
+"""
+
+from sys import argv
+
+def find_unqiue_element(array):
+    pass
+
+
+
+if __name__ == '__main__':
+    array = list(argv[1])
+
