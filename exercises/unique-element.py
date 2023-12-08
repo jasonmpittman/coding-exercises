7dc7c517b7aab29658aeff3b0bf6a5ba4dcd237a
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-08 05:30:42 -0500

    bitwise xor implemented

--- exercises/unique-element.py
+++ exercises/unique-element.py
@@ -16,6 +16,7 @@ Write a function that finds the element that appears exactly once in an array wh
     findUniqueElement([7, 3, 5, 5, 4, 3, 4]) should return 7.
 
     43243
+    7355434
     
 Started: Dec 08, 2023 @ 5:10am ET
 Intervals: 1
@@ -24,11 +25,26 @@ Ended: Dec 08, 2023 @ 5:40am ET
 
 from sys import argv
 
-def find_unqiue_element(array):
-    pass
+def find_unqiue_element(array: int) -> int:
+    """
+    1. Initialize Result: Start with a variable, say result, initialized to 0.
+    2. Iterate and XOR: Iterate through each element in the array. Perform a XOR operation between the result and the current element. Update result with this new value.
+        a. Operation: result = result ^ currentElement
+    3. Final Result: After completing the iteration, result will hold the value of the unique element. This happens because all elements that appear twice will cancel themselves out (due to the self-inverting property), leaving only the unique element.
+    """
+    result = 0
+    current = 0
+    
+    for i in map(int, array):
+        current = i
+        result = result ^ current
 
+    
+    return result
 
 
 if __name__ == '__main__':
     array = list(argv[1])
+    result = find_unqiue_element(array)
 
+    print(result)
