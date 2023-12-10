51b6969857f4c73036e79f139b9c1d370ed55b02
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-10 12:03:13 -0500

    implemented buffer

--- exercises/circular-buffer.py
+++ exercises/circular-buffer.py
@@ -26,23 +26,63 @@ Ended: Dec 09, 2023 @ 10:15am ET
 class CircularBuffer:
     def __init__(self, size):
         self.size = size
-        self.head # read
-        self.tail # write
+        self.head = 0 # read
+        self.tail = 0 # write
+        self.data = []
 
-    def read():
+    def read(self, buffer, index=None):
         """Return the element at the head position.
         Move the head position forward (circularly).
         Handle cases where the buffer is empty."""
+        if index is None:
+            print(buffer.data)
+        else:
+            return buffer[index]
 
-    def write():
+    def is_empty(buffer):
+        pass
+
+    def is_full(self, buffer, size):
+        if len(buffer.data) == size:
+            return True
+        else:
+            return False
+
+    # TODO: what happens when tail > size?
+    def write(self, buffer, value):
         """Add an element at the tail position.
         Move the tail position forward (circularly).
         If the buffer is full, either reject the new data or overwrite the oldest data (based on the mode set for the buffer)"""
+        if self.is_full(buffer, buffer.size):
+            raise Exception("Buffer is full.")
+        else:
+            buffer.data.insert(self.tail, value) 
+            self.tail += 1
 
-    def overwrite():
+
+    # TODO: what happens when head > size?
+    def overwrite(self, buffer, value):
        """If the buffer is full and a new element is written, overwrite the oldest element (at the head position)."""
+       buffer.data[self.head] = value
+       self.head += 1
+       
 
 
 
 if __name__ == '__main__':
-    pass
+    buffer = CircularBuffer(3)
+    buffer.read(buffer)
+    buffer.write(buffer, 10)
+    buffer.read(buffer)
+    buffer.write(buffer, 4)
+    buffer.read(buffer)
+    buffer.write(buffer, 7)
+    buffer.read(buffer)
+    
+    #buffer should be full
+    #buffer.write(buffer, 5)
+    #buffer.read(buffer)
+    
+    #this should overwrite where HEAD is
+    buffer.overwrite(buffer, 5)
+    buffer.read(buffer)
