fa315065ace84b91ab49bf57d8f76b95645bf7c6
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-11 03:11:01 -0500

    initial commit

---
+++ exercises/pancake-sort.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+"""Pancake Sort: https://old.reddit.com/r/dailyprogrammer/comments/np3sio/20210531_challenge_392_intermediate_pancake_sort/
+
+Implement the flipfront function. Given an array of integers and a number n between 2 and the length of the array (inclusive), return the array with the order of the first n elements reversed.
+
+flipfront([0, 1, 2, 3, 4], 2) => [1, 0, 2, 3, 4]
+flipfront([0, 1, 2, 3, 4], 3) => [2, 1, 0, 3, 4]
+flipfront([0, 1, 2, 3, 4], 5) => [4, 3, 2, 1, 0]
+flipfront([1, 2, 2, 2], 3) => [2, 2, 1, 2]
+
+Started: Dec 10, 2023 @ 3:15am ET
+Intervals: 1
+Ended: Dec 10, 2023 @ 3:30am ET
+
+"""
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
