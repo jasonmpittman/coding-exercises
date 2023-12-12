b6c092e7d67b9c6caedc31518091046fba885104
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-12 05:47:02 -0500

    added test cases

--- tests/test_pancake_sort.py
+++ tests/test_pancake_sort.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 __author__ = "Jason M. Pittman"
 __copyright__ = "Copyright 2023"
 __credits__ = ["Jason M. Pittman"]
@@ -15,17 +13,19 @@ try:
     from exercises import pancake_sort 
 except ImportError as e:
     print(e)
+# run from root as python3 -m unittest tests/test_pancake_sort.py
 
 class PancakeSortTest(unittest.TestCase):
     #01234,2 01234,3 01234,5
+
     def test_pancake_two(self):
-        self.assertEqual(pancake_sort.flip_front([0, 1, 2, 3, 4], 2), [1, 0, 2, 3, 4], 'The sort is correct')
+        self.assertEqual(pancake_sort.flip_front([0, 1, 2, 3, 4], 2), [1, 0, 2, 3, 4], 'The sort is incorrect')
 
-    #def test_pancake_three(self):
-    #    pass
+    def test_pancake_three(self):
+        self.assertEqual(pancake_sort.flip_front([0, 1, 2, 3, 4], 3), [2, 1, 0, 3, 4], 'The sort is incorrect')
 
-    #def test_pancake_five(self):
-    #    pass
+    def test_pancake_five(self):
+        self.assertEqual(pancake_sort.flip_front([0, 1, 2, 3, 4], 5), [4, 3, 2, 1, 0], 'The sort is incorrect')
 
 
 
