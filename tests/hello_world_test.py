50c535a09077a1633569c473a7ad2e815f19d756
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-12 04:49:47 -0500

    added metadata header

--- tests/hello_world_test.py
+++ tests/hello_world_test.py
@@ -1,3 +1,11 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
 import unittest
 
 try:
