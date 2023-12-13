826326c8e5ff8eecc488b53ee5cb018da02d46bd
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-13 05:29:35 -0500

    initial commit

---
+++ exercises/remove_duplicates.py
@@ -0,0 +1,37 @@
+
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+
+""" Remove Duplicates:
+
+Create an application that removes duplicate entries from a list of numbers. Your application will process a maximum of 20 integer numbers. Each number will be equal to or greater than zero. You do not need to edit for floating point numbers or invalid characters. Twenty is the maximum number of values to be entered. The user must be able to enter 0 numbers, 1 number, 20 numbers or any amount between 1 and 20. If you create a command-line application, allow the user to enter -1 to indicate that no more numbers will be entered. Once the input is complete, the application must indicate which numbers are duplicated and how many duplicates existed for each number.
+
+Sample:
+    Input: 1 2 3 4 5 1 6 1 7 1 8 1 9 1 10 -1
+    Output: Value 1: 5 copies are deleted
+                     1 2 3 4 5 6 7 8 9 10
+
+    Input: 1 2 1 2 1 2 1 2 3 4 5 3 -1
+    Output: Value 1: 3 copies are deleted
+            Value 2: 3 copies are deleted
+            Value 3: 1 copies are deleted
+                     1 2 3 4 5
+
+Started: Dec 13, 2023 @ 5:30am ET
+Intervals: 1
+Ended: Dec 13, 2023 @ 6:00am ET
+
+"""
+
+
+
+
+
+if __name__ == '__main__':
+    pass
