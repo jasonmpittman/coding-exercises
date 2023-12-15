8c7a59d85f6183ea6d2a0ddaa7ff060d44756fa3
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-15 05:05:50 -0500

    initial commit

---
+++ tests/test_list_merger.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Test List Merger
+
+Consider these scenarios:
+
+    Normal Case: Both lists contain elements in sorted order.
+    Empty Lists: Both lists are empty.
+    One Empty List: One list is empty, and the other is not.
+    Single Element Lists: Each list contains only one element.
+    Duplicate Elements: Lists contain some duplicate elements.
+    Negative Numbers: Lists contain negative numbers.
+    Large Numbers: Lists contain very large numbers.
+    Non-Integer Elements: (Optional) If your function should handle this, include lists with strings or other types.
+
+Started: Dec 15, 2023 @ 5:10am ET
+Intervals: 1
+Ended: Dec 15, 2023 @ 5:40am ET   
+"""
