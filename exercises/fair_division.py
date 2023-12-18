740053d36ad4e6333ce8d4e3f76216867c8dd14e
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-18 03:57:44 -0500

    initial commit

---
+++ exercises/fair_division.py
@@ -0,0 +1,35 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+
+
+""" Fair Division: https://icpc.global/worldfinals/problems/2021-ICPC-World-Finals/icpc2021.pdf
+
+Sample Input:
+    8 51000
+    6 91000
+    10 1000000000000000000
+
+Sample Output:
+    1 2
+    2 3
+    impossible
+
+Started: Dec 18, 2023 @ 4:00am ET
+Intervals: 1
+Ended: Dec 18, 2023 @ 4:30am ET 
+"""
+
+from sys import argv
+
+def divide_loot():
+    pass
+
+
+if __name__ == '__main__':
+    pass
