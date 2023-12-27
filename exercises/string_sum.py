1d6ed794030e072330d00e5c8b7b4c7fa89ae8c7
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-27 06:30:26 -0500

    initial commit

---
+++ exercises/string_sum.py
@@ -0,0 +1,20 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" String Sum:
+
+
+Started: Dec 27, 2023 @ 6:30am ET
+Intervals: 1
+Ended: Dec 27, 2023 @ 7:00am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
