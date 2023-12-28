481bbbb6c2f8be6df60dc1f27e10e0367647ccb0
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-28 06:41:49 -0500

    initial commit

---
+++ exercises/roman_to_int.py
@@ -0,0 +1,33 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Roman to Integer:
+Write a Python function that converts a Roman numeral to an integer. Roman numerals are represented by combinations of letters from the Latin alphabet (I, V, X, L, C, D, M) which represent values 1, 5, 10, 50, 100, 500, and 1000, respectively.
+
+Given a string representing a Roman numeral, convert it to an integer. The Roman numeral is guaranteed to be within the range from 1 to 3999 (I to MMMCMXCIX).
+
+Example:
+
+    Input: "IX"
+    Output: 9
+    Explanation: IX is a Roman numeral for 9.
+
+Tests:
+    III (3)
+    LVIII (58)
+    MCMXCIV (1994)
+
+Started: Dec 28, 2023 @ 6:45am ET
+Intervals: 1
+Ended: Dec 28, 2023 @ 7:15am ET
+"""
+from sys import argv
+
+
+if __name__ == '__main__':
+    pass
