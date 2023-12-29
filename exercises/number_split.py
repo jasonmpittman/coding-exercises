c5279e13dba308c2b61bbc5a72bc5f5e7ff136bb
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-29 06:01:01 -0500

    initial commit

---
+++ exercises/number_split.py
@@ -0,0 +1,29 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Number Split:
+
+Given a number, return a list containing the two halves of the number. If the number is odd, make the rightmost number higher.
+
+Examples:
+
+    number_split(4) ➞ [2, 2]
+    number_split(10) ➞ [5, 5]
+    number_split(11) ➞ [5, 6]
+    number_split(-9) ➞ [-5, -4]
+
+
+Started: Dec 29, 2023 @ 6:00am ET
+Intervals: 1
+Ended: Dec 29, 2023 @ 6:30am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
