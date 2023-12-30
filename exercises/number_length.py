2e65c378af6349d8d3cf66ca583893d0018a7c5f
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-30 05:41:16 -0500

    initial commit

---
+++ exercises/number_length.py
@@ -0,0 +1,27 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Number Length:
+
+Create a function that takes a number num and returns its length.
+
+Examples:
+    number_length(10) ➞ 2
+    number_length(5000) ➞ 4
+    number_length(0) ➞ 1
+
+
+Started: Dec 29, 2023 @ 5:45am ET
+Intervals: 1
+Ended: Dec 29, 2023 @ 6:15am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
