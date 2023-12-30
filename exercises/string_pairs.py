0357d4bddbd06467abfa661b2f058fba2f9ad43b
Jason M. Pittman <jason@jasonmpittman.com>
2023-12-30 05:49:57 -0500

    initial commit

---
+++ exercises/string_pairs.py
@@ -0,0 +1,27 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" String Pairs:
+
+Create a function that takes a string s and returns a list of two-paired characters. If the string has an odd number of characters, add an asterisk * in the final pair.
+
+Examples:
+
+    string_pairs("mubashir") ➞ ["mu", "ba", "sh", "ir"]
+    string_pairs("edabit") ➞ ["ed", "ab", "it"]
+    string_pairs("airforces") ➞ ["ai", "rf", "or", "ce", "s*"]
+
+Started: Dec 30, 2023 @ 5:50am ET
+Intervals: 1
+Ended: Dec 30, 2023 @ 6:20am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
