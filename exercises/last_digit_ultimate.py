7d6b0d82e9dd1ffd587fb172070c93f83893c023
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-01 06:39:30 -0500

    initial commit

---
+++ exercises/last_digit_ultimate.py
@@ -0,0 +1,41 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+"""Last Digit Ultimate:
+Your job is to create a function, that takes 3 numbers: a, b, c and returns True if the last digit of a * b = the last digit of c. Check the examples below for an explanation.
+
+Examples:
+
+last_dig(25, 21, 125) ➞ True
+# The last digit of 25 is 5, the last digit of 21 is 1, and the last
+# digit of 125 is 5, and the last digit of 5*1 = 5, which is equal
+# to the last digit of 125(5).
+
+last_dig(55, 226, 5190) ➞ True
+# The last digit of 55 is 5, the last digit of 226 is 6, and the last
+# digit of 5190 is 0, and the last digit of 5*6 = 30 is 0, which is
+# equal to the last digit of 5190(0).
+
+last_dig(12, 215, 2142) ➞ False
+# The last digit of 12 is 2, the last digit of 215 is 5, and the last
+# digit of 2142 is 2, and the last digit of 2*5 = 10 is 0, which is
+# not equal to the last digit of 2142(2).
+
+
+Started: Jan 01, 2024 @ 6:45am ET
+Intervals: 1
+Ended: Jan 01, 2024 @ 7:15am ET
+"""
+from sys import argv
+
+
+
+
+
+if __name__ == '__main__':
+    pass
