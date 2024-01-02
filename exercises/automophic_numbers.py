4e528a7f0aa1aa3d4cac68e7c052712a13123918
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-02 05:42:06 -0500

    initial commit

---
+++ exercises/automophic_numbers.py
@@ -0,0 +1,27 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Automorphic Numbers:
+Create a function that takes a number and returns true if the number is automorphic, false if it isn't.
+
+A number nis automorphic if n^2ends in n
+
+Example:
+    isAutomorphic(5) ➞ true
+    isAutomorphic(8) ➞ false
+    isAutomorphic(76) ➞ true
+
+Started: Jan 02, 2024 @ 5:45am ET
+Intervals: 1
+Ended: Jan 02, 2024 @ 6:15am ET
+"""
+from sys import argv
+
+
+if __name__ == '__main__':
+    pass
