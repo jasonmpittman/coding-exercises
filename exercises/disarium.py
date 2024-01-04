1952f5a68e334e5596e9b97d515a705b72e9699f
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-04 05:58:38 -0500

    initial commit

---
+++ exercises/disarium.py
@@ -0,0 +1,23 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Disarium:
+A number is said to be Disarium if the sum of its digits raised to their respective positions is the number itself.
+
+Create a function that determines whether a number is a Disarium or not.
+
+
+Started: Jan 04, 2024 @ 6:00am ET
+Intervals: 1
+Ended: Jan 04, 2024 @ 6:30am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
