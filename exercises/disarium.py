491d720b6dd17e3b7609587894a11aab710cdabb
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-04 06:08:17 -0500

    implemented disarium test for n

--- exercises/disarium.py
+++ exercises/disarium.py
@@ -11,13 +11,47 @@ A number is said to be Disarium if the sum of its digits raised to their respect
 
 Create a function that determines whether a number is a Disarium or not.
 
+Examples:
+    is_disarium(75) ➞ False
+    # 7^1 + 5^2 = 7 + 25 = 32
+    is_disarium(135) ➞ True
+    # 1^1 + 3^2 + 5^3 = 1 + 9 + 125 = 135
+
+    is_disarium(544) ➞ False
+    is_disarium(518) ➞ True
+    is_disarium(466) ➞ False
+    is_disarium(8) ➞ True
 
 Started: Jan 04, 2024 @ 6:00am ET
 Intervals: 1
-Ended: Jan 04, 2024 @ 6:30am ET
+Ended: Jan 04, 2024 @ 6:07am ET
 """
+from sys import argv
 
+def convert_to_numbers(n: str) -> list:
+    numbers = []
 
+    for number in n:
+        numbers.append(int(number))
+    
+    return numbers
+
+def is_disarium(n):
+    total = 0
+    e = 1
+    numbers = convert_to_numbers(n)
+
+    for number in numbers:
+        total += number ** e
+        e += 1
+
+    if total == int(n):
+        return True
+    else:
+        return False
+    
 
 if __name__ == '__main__':
-    pass
+    n = argv[1]
+
+    print(is_disarium(n))
