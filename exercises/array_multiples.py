113f4da5d2b9d2ba822d1c30cc6913bb7da12680
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-05 03:32:31 -0500

    initial commit

---
+++ exercises/array_multiples.py
@@ -0,0 +1,26 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Array Multiples:
+Create a function that takes two numbers as arguments (num, length) and returns an array of multiples of num until the array length reaches length.
+
+Examples:
+    ArrayOfMultiples(7, 5) ➞ [7, 14, 21, 28, 35]
+    ArrayOfMultiples(12, 10) ➞ [12, 24, 36, 48, 60, 72, 84, 96, 108, 120]
+    ArrayOfMultiples(17, 6) ➞ [17, 34, 51, 68, 85, 102]
+
+Started: Jan 05, 2024 @ 3:35am ET
+Intervals: 1
+Ended: Jan 05, 2024 @ 4:05am ET
+"""
+
+from sys import argv
+
+
+if __name__ == '__main__':
+    pass
