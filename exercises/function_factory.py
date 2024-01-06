392581adf35190c6b425c48aabb27379b961cde2
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-06 14:06:59 -0500

    iniital commit

---
+++ exercises/function_factory.py
@@ -0,0 +1,29 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Function Factory:
+
+Create a function that takes a "base number" as an argument. This function should return another function which takes a new argument, and returns the sum of the "base number" and the new argument.
+
+Examples:
+
+    plusFive = makePlusFunction(5)
+    plusFive(2) ➞ 7
+    plusFive(-8) ➞ -3
+
+    plusTen = makePlusFunction(10)
+    plusTen(0) ➞ 10
+    plusTen(188) ➞ 198
+    plusFive(plusTen(0)) ➞ 15
+
+"""
+
+
+
+if __name__ == '__main__':
+    pass
