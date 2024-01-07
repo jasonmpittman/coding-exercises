e4dafff9f399ce866f5e24718fe3b5032a551c30
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-07 06:56:38 -0500

    initial commit

---
+++ exercises/triangular_numbers.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Triangular Numbers:
+Write a function that returns the number of dots when given its corresponding triangle number of the sequence.
+
+Examples:
+    triangle(1) ➞ 1
+    triangle(6) ➞ 21
+    triangle(215) ➞ 23220
+
+Started: Jan 07, 2024 @ 7:00am ET
+Intervals: 1
+Ended: Jan 07, 2024 @ 7:30am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
