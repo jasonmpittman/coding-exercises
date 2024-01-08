e76bc02bd33e4cbdcf266420d55783acadb262d8
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-08 05:24:07 -0500

    simple 2n-1 function

--- exercises/tower_of_hanoi.py
+++ exercises/tower_of_hanoi.py
@@ -7,14 +7,25 @@ __maintainer__ = "Jason M. Pittman"
 __status__ = "Exercise"
 
 """ Tower of Hanoi:
+Create a function that takes a number discs as an argument and returns the minimum amount of steps needed to complete the game.
 
+Examples:
+    towerHanoi(3) ➞ 7
+    towerHanoi(5) ➞ 31
+    towerHanoi(0) ➞ 0
 
 Started: Jan 08, 2024 @ 5:15am ET
 Intervals: 1
-Ended: Jan 08, 2024 @ 5:45am ET
+Ended: Jan 08, 2024 @ 5:20am ET
 """
+from sys import argv
 
+def count_number_of_steps(n: int) -> int:
+    return 2**n - 1
 
 
 if __name__ == '__main__':
-    pass
+    n = int(argv[1])
+    number_of_steps = count_number_of_steps(n)
+
+    print(number_of_steps)
