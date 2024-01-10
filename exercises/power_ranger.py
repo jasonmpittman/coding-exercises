6d6c13738211142d39fbe22804fb88be89ebca70
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-10 05:52:46 -0500

    initial commit

---
+++ exercises/power_ranger.py
@@ -0,0 +1,35 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+
+""" Power Range:
+Create a function that takes in n, a, b and returns the number of positive values raised to the nth power that lie in the range [a, b], inclusive.
+
+Examples:
+    power_ranger(2, 49, 65) ➞ 2
+    # 2 squares (n^2) lie between 49 and 65, 49 (7^2) and 64 (8^2)
+
+    power_ranger(3, 1, 27) ➞ 3
+    # 3 cubes (n^3) lie between 1 and 27, 1 (1^3), 8 (2^3) and 27 (3^3)
+
+    power_ranger(10, 1, 5) ➞ 1
+    # 1 value raised to the 10th power lies between 1 and 5, 1 (1^10)
+
+    power_ranger(5, 31, 33) ➞ 1
+    power_ranger(4, 250, 1300) ➞ 3
+
+
+Started: Jan 10, 2024 @ 6:00am ET
+Intervals: 1
+Ended: Jan 10, 2024 @ 6:30am ET
+"""
+from sys import argv
+
+
+if __name__ == '__main__':
+    pass
