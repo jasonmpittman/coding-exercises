8fad47a58d00ea33b138a8a14fcf4bc151d77e1d
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-11 06:09:02 -0500

    initial commit

---
+++ exercises/prime_search.py
@@ -0,0 +1,29 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Prime Search:
+
+Create a function that finds a target number in a list of prime numbers. Implement a binary search algorithm in your function. The target number will be from 2 through 97. If the target is prime then return "yes" else return "no".
+
+Examples:
+primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+is_prime(primes, 3) ➞ "yes"
+is_prime(primes, 4) ➞ "no"
+is_prime(primes, 67) ➞ "yes"
+is_prime(primes, 36) ➞ "no"
+
+Started: Jan 11, 2024 @ 6:15am ET
+Intervals: 1
+Ended: Jan 11, 2024 @ 6:45am ET
+"""
+
+
+
+if __name__ == '__main__':
+    primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
