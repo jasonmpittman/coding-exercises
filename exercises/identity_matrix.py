7233dc4555980880d783bf9eeba84bf9adb91fb4
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-12 04:20:10 -0500

    initial commit

---
+++ exercises/identity_matrix.py
@@ -0,0 +1,33 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Identity Matrix:
+Create a function that takes an integer n and returns the identity matrix of n x n dimensions. For this challenge, if the integer is negative, return the mirror image of the identity matrix of n x n dimensions.. It does not matter if the mirror image is left-right or top-bottom.
+
+Examples:
+    id_mtrx(2) ➞ [
+    [1, 0],
+    [0, 1]
+    ]
+
+    id_mtrx(-2) ➞ [
+    [0, 1],
+    [1, 0]
+    ]
+
+    id_mtrx(0) ➞ []
+
+Started: Jan 12, 2024 @ 4:30am ET
+Intervals: 1
+Ended: Jan 12, 2024 @ 5:00am ET
+"""
+from sys import argv
+
+
+if __name__ == '__main__':
+    pass
