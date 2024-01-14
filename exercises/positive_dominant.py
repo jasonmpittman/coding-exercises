ca19254dc980e2ac2b0895b0c4aa56d313a0a651
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-14 05:48:04 -0500

    initial commit

---
+++ exercises/positive_dominant.py
@@ -0,0 +1,29 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Positive Dominant:
+An array is positive dominant if it contains strictly more unique positive values than unique negative values. Write a function that returns true if an array is positive dominant.
+
+Examples:
+    isPositiveDominant([1, 1, 1, 1, -3, -4]) ➞ false
+    // There is only 1 unique positive value (1).
+    // There are 2 unique negative values (-3, -4).
+
+    isPositiveDominant([5, 99, 832, -3, -4]) ➞ true
+    isPositiveDominant([5, 0]) ➞ true
+    isPositiveDominant([0, -4, -1]) ➞ false
+
+Started: Jan 14, 2024 @ 5:45am ET
+Intervals: 1
+Ended: Jan 14, 2024 @ 6:15am ET
+"""
+from sys import argv
+
+
+if __name__ == '__main__':
+    pass
