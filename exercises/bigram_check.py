bfdebdfd4d56b3ad1dec7626e32005dbc88461d7
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-16 06:00:45 -0500

    implemented bigram check function

--- exercises/bigram_check.py
+++ exercises/bigram_check.py
@@ -22,10 +22,31 @@ Examples:
 
 Started: Jan 16, 2024 @ 5:40am ET
 Intervals: 1
-Ended: Jan 16, 2024 @ 6:10am ET
+Ended: Jan 16, 2024 @ 6:00am ET
 """
+from sys import argv
 
+def check_bigrams(bigrams : list, strings: list):
+    match_map = {}
+
+    for bigram in bigrams:
+        for string in strings:    
+            if bigram in string:
+                match_map.update({bigram: 1})
+
+    if len(bigrams) == len(match_map.keys()): # this feels hacky but it works
+        return True
+    else:
+        return False             
 
 
 if __name__ == '__main__':
-    pass
+    # at,be,th,au beautiful,the,hat -> true
+    # ay,be,ta,cu maybe,beta,abet,course -> false
+    # th,fo,ma,or the,many,for,forest -> true
+
+    bigrams = argv[1].split(',')
+    strings = argv[2].split(',')
+
+    print(check_bigrams(bigrams, strings))
+    
