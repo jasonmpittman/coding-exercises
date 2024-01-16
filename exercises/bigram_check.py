be641752e062afe7bad4e39ee1f0a73a09772ae1
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-16 05:37:23 -0500

    initial commit

---
+++ exercises/bigram_check.py
@@ -0,0 +1,31 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Bigram Check:
+You are given an input array of bigrams, and an array of words.
+
+Write a function that returns true if every single bigram from this array can be found at least once in an array of words.
+
+Examples:
+    canFind(["at", "be", "th", "au"], ["beautiful", "the", "hat"]) ➞ true
+
+    canFind(["ay", "be", "ta", "cu"], ["maybe", "beta", "abet", "course"]) ➞ false
+    # "cu" does not exist in any of the words.
+
+    canFind(["th", "fo", "ma", "or"], ["the", "many", "for", "forest"]) ➞ true
+    canFind(["oo", "mi", "ki", "la"], ["milk", "chocolate", "cooks"]) ➞ false
+
+Started: Jan 16, 2024 @ 5:40am ET
+Intervals: 1
+Ended: Jan 16, 2024 @ 6:10am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
