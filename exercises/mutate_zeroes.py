3da72460772b2d684051aca0c91600eb33755625
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-18 07:14:37 -0500

    implemented mutation function

--- exercises/mutate_zeroes.py
+++ exercises/mutate_zeroes.py
@@ -10,7 +10,7 @@ __status__ = "Exercise"
 Write a function that moves all the zeroes to the end of a list. Do this without returning a copy of the input list.
     You must mutate the original list.
     Keep the relative order of the non-zero elements the same.
-    
+
 Examples:
     zeroes_to_end([1, 2, 0, 0, 4, 0, 5]) ➞ [1, 2, 4, 5, 0, 0, 0]
     zeroes_to_end([0, 0, 2, 0, 5]) ➞ [2, 5, 0, 0, 0]
@@ -19,10 +19,23 @@ Examples:
 
 Started: Jan 18, 2024 @ 7:00am ET
 Intervals: 1
-Ended: Jan 18, 2024 @ 7:30am ET
+Ended: Jan 18, 2024 @ 7:13am ET
 """
+from sys import argv
 
-
+def mutate_list(l : list) -> list:
+    
+    for c in l:
+        if c == "0":
+            l.remove(c)
+            l.insert(len(l), c)
+        
+    print("After mutation: ", l)
+    
 
 if __name__ == '__main__':
-    pass
+    # test lists: 1,2,0,0,4,0,5 ; 0,0,2,0,5
+    l = argv[1].split(',') 
+    print("Before mutation: ", l)
+    
+    mutate_list(l)
