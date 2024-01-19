c4c4c4484ccbf8ffe8928156cc9f88eb2854fd3d
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-19 06:11:44 -0500

    initial commit

---
+++ exercises/first_letter.py
@@ -0,0 +1,31 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" First Letter:
+Given a sentence, create a function which shifts the first letter of each word to the next word in the sentence (shifting right).
+
+Examples:
+    shift_sentence("create a function") ➞ "freate c aunction"
+    shift_sentence("it should shift the sentence") ➞ "st ihould shift she tentence"
+    shift_sentence("the output is not very legible") ➞ "lhe tutput os iot nery vegible"
+    shift_sentence("edabit") ➞ "edabit"
+
+Notes:
+    The last word shifts its first letter to the first word in the sentence.
+    All sentences will be given in lowercase.
+    Note how single words remain untouched (example #4).
+
+Started: Jan 19, 2024 @ 6:15am ET
+Intervals: 1
+Ended: Jan 19, 2024 @ 6:45am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
