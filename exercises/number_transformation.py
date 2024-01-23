eb9d22d58fcd1aba50252858803eb9c0e795e540
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-23 05:41:42 -0500

    initial commit

---
+++ exercises/number_transformation.py
@@ -0,0 +1,38 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Number Transformation:
+
+You are given two positive integers n and m. You have to perform some basic mathematical operations on n to obtain m. These are your options:
+
+(n-1)/2   - if (n-1) is divisible by 2
+n/2       - if n is divisible by 2
+n*2
+
+Create a function that returns the minimum number of steps required to transform n into m.
+
+Examples:
+    number_transform(2, 8) ➞ 2
+    // 2 * 2 = 4
+    // 4 * 2 = 8
+
+    number_transform(9, 2) ➞ 2
+    // (9-1) / 2 = 4
+    // 4 / 2 = 2
+
+    number_transform(1024, 1024) ➞ 0
+
+Started: Jan 23, 2024 @ 5:45am ET
+Intervals: 1
+Ended: Jan 23, 2024 @ 6:15am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
