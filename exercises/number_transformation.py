6ba7de7aba4b93ff4060906b996f24027eaccc21
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-23 05:56:36 -0500

    implemented transformation function

--- exercises/number_transformation.py
+++ exercises/number_transformation.py
@@ -31,8 +31,32 @@ Started: Jan 23, 2024 @ 5:45am ET
 Intervals: 1
 Ended: Jan 23, 2024 @ 6:15am ET
 """
+from sys import argv
 
+# TODO: 2,8 or similar causes an inifinite loop. the problems is flawed. 9,2 works as expected.
+def transform_number(n: int, m: int):
+    steps = 0
 
+    while n != m:
+        if n < m:
+            print("Entering case 0")
+            n = n * 2
+            steps += 1
+        if (n - 1) % 2 == 0:
+            print("Entering case 1")
+            n = (n - 1) / 2
+            steps += 1
+        elif n % 2 == 0:
+            print("Entering case 2")
+            n = n / 2
+            steps += 1
+        else:
+            print("Entering case 3")
+            n = n * 2
+            steps += 1
+
+    print(steps)
 
 if __name__ == '__main__':
-    pass
+    n, m = argv[1].split(',')
+    transform_number(int(n), int(m))
