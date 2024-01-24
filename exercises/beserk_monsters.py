4482da9499dc6a98864ba8c4ea514dac93b49f96
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-24 05:18:18 -0500

    initial commit

---
+++ exercises/beserk_monsters.py
@@ -0,0 +1,22 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Beserk Monsters: https://codeforces.com/problemset/problem/1922/D
+
+
+
+Started: Jan 24, 2024 @ 5:15am ET
+Intervals: 1
+Ended: Jan 24, 2024 @ 5:45am ET
+"""
+from sys import argv
+
+
+
+if __name__ == '__main__':
+    pass
