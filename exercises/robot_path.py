6acf4a5acdecfdf39d7c9c25ff209448d1e8e94d
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-25 06:38:06 -0500

    initial commit

---
+++ exercises/robot_path.py
@@ -0,0 +1,34 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Robot Path: https://cses.fi/problemset/task/1742/
+
+
+Example:
+    Input:
+    5
+    U 2
+    R 3
+    D 1
+    L 5
+    U 2
+
+    Output:
+    9
+
+
+
+Started: Jan 25, 2024 @ 6:40am ET
+Intervals: 1
+Ended: Jan 25, 2024 @ 7:10am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
