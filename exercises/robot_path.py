305b6cdde27b34d3d9d22181eac4dcd12b1fa023
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-25 07:30:21 -0500

    implemented move robot function

--- exercises/robot_path.py
+++ exercises/robot_path.py
@@ -22,13 +22,62 @@ Example:
     9
 
 
-
 Started: Jan 25, 2024 @ 6:40am ET
 Intervals: 1
 Ended: Jan 25, 2024 @ 7:10am ET
 """
+from sys import argv
+import json
+
+def is_visited(visited_coordinates: list, current_coordinates: set) -> bool:
+    if current_coordinates in visited_coordinates:
+        return True
+    else:
+        return False
+
+#TODO: implement is_visited check
+def move_robot(robot_commands: dict) -> int:
+    move_count = 0
+    x, y = 0, 0
+    visited_coordinates = [(0, 0)]
 
 
+    for key, value in robot_commands.items():
+        print(key, "->", value)
+        
+        if key == "U":
+            y = y + value
+            
+            visited_coordinates.append((x, y))
+            move_count += value
 
-if __name__ == '__main__':
-    pass
+        elif key == "D":
+            y = y - value
+
+            visited_coordinates.append((x, y))
+            move_count += value
+
+        elif key == "L":
+            x = x - value
+            
+            visited_coordinates.append((x, y))
+            move_count += value
+
+        elif key == "R":
+            x = x + value
+            
+            visited_coordinates.append((x, y))
+            move_count += value
+        else:
+            pass
+        
+        print(visited_coordinates)
+
+    return move_count
+
+if __name__ == "__main__":
+    # '{"N":5, "U": 2, "R": 3, "D": 1, "L": 5, "U": 2}'
+    robot_commands = json.loads(argv[1])
+    
+    r = move_robot(robot_commands)
+    print(r)
