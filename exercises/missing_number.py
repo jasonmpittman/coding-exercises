6d950288d8e621840e44e4ac66d13e0326bf41a9
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-27 05:31:49 -0500

    implement missing number function

--- exercises/missing_number.py
+++ exercises/missing_number.py
@@ -8,13 +8,53 @@ __status__ = "Exercise"
 
 """ Missing Number: https://cses.fi/problemset/task/1083/
 
+You are given all numbers between 1,2,\ldots,n except one. Your task is to find the missing number.
+ß
+Input
+
+    The first input line contains an integer n.
+
+    The second line contains n-1 numbers. Each number is distinct and between 1 and n (inclusive).
+    Output
+
+    Print the missing number.
+
+Constraints
+    2 <= n <= 2 * 10^5
+
+Example
+    Input:
+    5
+    2 3 1 5
+
+    Output:
+    4
 
 Started: Jan 27, 2024 @ 5:20am ET
 Intervals: 1
 Ended: Jan 27, 2024 @ 5:50am ET
 """
+from sys import argv
 
+def find_missing_number(n: int, numbers: list) -> int:
+    missing_number = 0
+
+    expected_sum = (n * (n + 1)) // 2
+    real_sum = 0
+
+    # could use the sum() method here
+    for i in numbers:
+        real_sum += i
+
+    missing_number = expected_sum - real_sum 
+
+    return missing_number
 
 
 if __name__ == '__main__':
-    pass
+    # 5 "2,3,1,5"
+    n = int(argv[1])
+    numbers = [int(x) for x in argv[2].split(',')] 
+
+    print(find_missing_number(n, numbers))
+    
