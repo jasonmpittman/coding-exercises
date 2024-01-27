a60ef7f8271ea7e35ae01189e1f0961084038df0
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-27 05:15:54 -0500

    initial commit

---
+++ exercises/missing_number.py
@@ -0,0 +1,20 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Missing Number: https://cses.fi/problemset/task/1083/
+
+
+Started: Jan 27, 2024 @ 5:20am ET
+Intervals: 1
+Ended: Jan 27, 2024 @ 5:50am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
