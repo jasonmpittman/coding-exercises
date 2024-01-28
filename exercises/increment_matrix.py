d6c24374b602f7343fe4e304f7495032a7c74f4f
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-28 14:17:32 -0500

    implemented increment matrix function

--- exercises/increment_matrix.py
+++ exercises/increment_matrix.py
@@ -41,8 +41,40 @@ Started: Jan 28, 2024 @ 1:45pm ET
 Intervals: 1
 Ended: Jan 28, 2024 @ 2:15pm ET
 """
+from sys import argv
 
+# TODO: works for 2x2 but 3x3 gets a index out of range on line 70 for 'index'
+def increment_matrix(number_of_rows: int, number_of_columns: int, increments: list) -> list:
+    matrix = []
 
+    # build the base matrix...is this optimal?
+    for r in range(number_of_rows):
+        row = []
+        for c in range(number_of_columns):
+            row.append(0)
+        matrix.append(row)
+
+    # loop over increments
+    for increment in increments:
+        # if increment is r then add 1 to index
+        if 'r' in increment:
+            index = int(increment[0])
+
+            for i in range(len(matrix)):
+                matrix[index][i] += 1
+
+        # if increment is c then add 1 to index
+        if 'c' in increment:
+            index = int(increment[0])
+        
+            for i in range(len(matrix)):
+                matrix[i][index] += 1
+            
+    print(matrix)
 
 if __name__ == '__main__':
-    pass
+    number_of_rows = argv[1]
+    number_of_columns = argv[2]
+    increments = argv[3].split(',') # 3 3 "1c,2c,2c,3c,3c,3c"
+
+    increment_matrix(int(number_of_rows), int(number_of_columns), increments)
