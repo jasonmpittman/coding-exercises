17c1e709a238966cc4e4b4744ff6a18677303a5d
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-28 13:44:05 -0500

    initial commit

---
+++ exercises/increment_matrix.py
@@ -0,0 +1,48 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Increment Matrix:
+Write a function that takes in three parameters: r, c, i, where:
+
+    r and c are the number of rows and columns to initialize a zero matrix.
+    i represents the list of incrementing operations (+1).
+
+And returns the resulting matrix after applying all the increment operations. Each increment operation will add 1 to the rows or columns specified in the incrementing list.
+
+Examples:
+    final(2, 2, ["0r", "0r", "0r", "1c"]) ➞ [
+    [3, 4],
+    [0, 1]
+    ]
+
+    final(2, 2, ["0c"]) ➞ [
+    [1, 0],
+    [1, 0]
+    ]
+
+    final(3, 3, ["1c", "2c", "2c", "3c", "3c", "3c"]) ➞ [
+        [1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3]
+    ]
+
+    final(3, 3, []) ➞ [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+    ]
+
+Started: Jan 28, 2024 @ 1:45pm ET
+Intervals: 1
+Ended: Jan 28, 2024 @ 2:15pm ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
