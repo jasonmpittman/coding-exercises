580a132582b7252a7d88627ec2bf710c4d7f9742
Jason M. Pittman <jason@jasonmpittman.com>
2024-01-29 05:03:45 -0500

    initial commit

---
+++ exercises/persistence.py
@@ -0,0 +1,27 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Persistence:
+The additive persistence of an integer, n, is the number of times you have to replace n with the sum of its digits until n becomes a single digit integer.
+
+The multiplicative persistence of an integer, n, is the number of times you have to replace n with the product of its digits until n becomes a single digit integer.
+
+Create two functions that take an integer as an argument and:
+
+    Return its additive persistence.
+    Return its multiplicative persistence.
+
+Started: Jan 29, 2024 @ 5:05am ET
+Intervals: 1
+Ended: Jan 29, 2024 @ 5:35am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
