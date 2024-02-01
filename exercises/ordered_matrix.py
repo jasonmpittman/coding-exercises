8d0a3abc683899939b7aa74a6c17cf1f2bf6bef8
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-01 06:21:27 -0500

    initial commit

---
+++ exercises/ordered_matrix.py
@@ -0,0 +1,32 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Ordered Matrix:
+Create an ordered 2D array (matrix). A matrix is ordered if its (0, 0) element is 1, its (0, 1) element is 2, and so on. Your function needs to create an a × b matrix. a is the first argument and b is the second.
+
+Examples:
+    orderedMatrix(5, 5) ➞ [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20],
+    [21, 22, 23, 24, 25]
+    ]
+
+    orderedMatrix(1, 1) ➞ [[1]]
+    orderedMatrix(1, 5) ➞ [[1, 2, 3, 4, 5]]
+
+Started: Feb 01, 2024 @ 6:25am ET
+Intervals: 1
+Ended: Feb 01, 2024 @ 6:55am ET    
+"""
+
+
+
+if __name__ == '__main__':
+    pass
