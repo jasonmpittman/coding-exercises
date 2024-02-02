c40c3824152cab1ce81b1e875fa9ae9428a8aacb
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-02 03:56:06 -0500

    initial commit

---
+++ exercises/legendre.py
@@ -0,0 +1,27 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Legendre's Formula: https://edabit.com/challenge/2mtNE8cRcctgBCPZw
+
+Legendre's formula finds the exponent of the largest power of some prime p that divides (is a factor of) the factorial of some number n.
+
+Examples:
+    legendre(5, 100) ➞ 24
+    legendre(2, 128) ➞ 127
+    legendre(3, 50) ➞ 22
+
+
+Started: Feb 02, 2024 @ 4:00am ET
+Intervals: 1
+Ended: Feb 02, 2024 @ 4:30am ET      
+"""
+
+
+
+if __name__ == '__main__':
+    pass
