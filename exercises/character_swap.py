9e0f897d138302dd9eb495c557b9dad6a3453cb1
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-04 04:26:51 -0500

    initial commit

---
+++ exercises/character_swap.py
@@ -0,0 +1,26 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+
+""" Character Swap:
+
+Write a function to replace all instances of character c1 with character c2 and vice versa.
+
+Examples:
+    doubleSwap( "aabbccc", "a", "b") ➞ "bbaaccc"
+
+    doubleSwap("random w#rds writt&n h&r&", "#", "&")
+    ➞ "random w&rds writt#n h#r#"
+
+    doubleSwap("128 895 556 788 999", "8", "9")
+    ➞ "129 985 556 799 888"
+
+Started: Feb 04, 2024 @ 4:30am ET
+Intervals: 1
+Ended: Feb 04, 2024 @ 5:00am ET
+"""
