ed1b669d6de768ee0ba60fd8eeda7e86305e0858
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-05 05:55:28 -0500

    implemented transpose function

--- exercises/transpose_matrix.py
+++ exercises/transpose_matrix.py
@@ -31,10 +31,35 @@ Example:
 
 Started: Feb 05, 2024 @ 5:35am ET
 Intervals: 1
-Ended: Feb 05, 2024 @ 5:55am ET
+Ended: Feb 05, 2024 @ 5:54am ET
 """
 
+def transpose_matrix(matrix: list) -> list:
+    number_of_rows = len(matrix)
+    number_of_columns = len(matrix[0])
+    transposed_matrix = []
 
+    for i in range(number_of_columns):
+        new_row = []
+        for j in range(number_of_rows):
+            new_row.append(matrix[j][i])
+        
+        transposed_matrix.append(new_row)
+    
+    return transposed_matrix
 
 if __name__ == '__main__':
-    pass
+    matrix = [
+    [1, 1, 1],
+    [2, 2, 2],
+    [3, 3, 3]
+    ]
+
+    '''matrix = [
+    [5, 5],
+    [6, 7],
+    [9, 1]
+    ]'''
+
+    transposed_matrix = transpose_matrix(matrix)
+    print(transposed_matrix)
