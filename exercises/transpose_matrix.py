f40a2c6101ae9af6e876f0aee336e60a6ba0d81a
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-05 05:35:26 -0500

    initial commit

---
+++ exercises/transpose_matrix.py
@@ -0,0 +1,40 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Transpose Matrix:
+Create a function that transposes a 2D matrix.
+
+Example:
+    transposeMatrix([
+    [1, 1, 1],
+    [2, 2, 2],
+    [3, 3, 3]
+    ]) ➞ [
+    [1, 2, 3],
+    [1, 2, 3],
+    [1, 2, 3]
+    ]
+
+    transposeMatrix([
+    [5, 5],
+    [6, 7],
+    [9, 1]
+    ]) ➞ [
+    [5, 6, 9],
+    [5, 7, 1]
+    ]
+
+Started: Feb 05, 2024 @ 5:35am ET
+Intervals: 1
+Ended: Feb 05, 2024 @ 5:55am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
