e7e8d4c2572af67d772fa7987036f5608dfdfc36
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-06 06:31:10 -0500

    initial commit

---
+++ exercises/frequency_sort.py
@@ -0,0 +1,20 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Frequency Sort: 
+Create a function that takes an array of integers arr and sort the elements of the array by decreasing frequency of the elements. If two elements have the same frequency, sort them by increasing value.
+
+Examples:
+    sortFreq([2, 3, 5, 3, 7, 9, 5, 3, 7]) ➞ [3, 3, 3, 5, 5, 7, 7, 2, 9]
+    sortFreq([1, 2, 3, 0, 5, 0, 1, 6, 8, 8, 6, 9, 1]) ➞ [1, 1, 1, 0, 0, 6, 6, 8, 8, 2, 3, 5, 9]
+    sortFreq([4, 4, 2, 5, 1, 1, 3, 3, 2, 8]) ➞ [1, 1, 2, 2, 3, 3, 4, 4, 5, 8]
+
+Started: Feb 06, 2024 @ 6:30am ET
+Intervals: 1
+Ended: Feb 06, 2024 @ 7:00am ET
+"""
