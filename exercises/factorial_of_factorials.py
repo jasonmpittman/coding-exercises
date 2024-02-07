42610a01f5ebe777c172c8a14a91abc0deb7b089
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-07 04:50:05 -0500

    initial commit

---
+++ exercises/factorial_of_factorials.py
@@ -0,0 +1,20 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Factorial of Factorials:
+
+
+Started: Feb 07, 2024 @ 4:50am ET
+Intervals: 1
+Ended: Feb 07, 2024 @ 5:20am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
