e4a620922d84f54ff8428244baa09b45d048e90d
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-08 04:29:13 -0500

    initial commit

---
+++ exercises/occurrences.py
@@ -0,0 +1,37 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Occurrences:
+
+Write a Python program to find a list of integers with exactly two occurrences of nineteen and at least three occurrences of five
+
+Examples:
+    Input:
+    [19, 19, 15, 5, 3, 5, 5, 2]
+    Output:
+    True
+    Input:
+    [19, 15, 15, 5, 3, 3, 5, 2]
+    Output:
+    False
+    Input:
+    [19, 19, 5, 5, 5, 5, 5]
+    Output:
+    True
+
+Started: Feb 08, 2024 @ 4:30am ET
+Intervals: 1
+Ended: Feb 08, 2024 @ 5:00am ET
+"""
+
+
+
+
+
+if __name__ == '__main__':
+    pass
