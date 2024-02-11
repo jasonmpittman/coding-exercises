7ecc646fd02c83ddfe3b981e2d0fc507912b441b
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-11 11:39:08 -0500

    implemented gauss add function

--- exercises/gauss_addition.py
+++ exercises/gauss_addition.py
@@ -19,9 +19,27 @@ Started: Feb 11, 2024 @ 11:00am ET
 Intervals: 1
 Ended: Feb 11, 2024 @ 11:30am ET
 """
+from sys import argv
 
+# formula
+# add first + last n ... 1 + 100 = 101
+# cut input in half = 1...100 is a 50-51 or 100 // 2 is 50 and 101 - 50 is 51
+# l x h -> total or 50 x 101 = 5050
 
+# TODO: correct for 1...100 but not for others. need to troubleshoot
+def add(n, m) -> int:
+    first_number = n
+    last_number = m
+    sum_number = first_number + last_number
 
+    dividend = sum_number // 2
+    gauss_number = dividend * sum_number
+
+    return gauss_number
 
 if __name__ == '__main__':
-    pass
+    n = int(argv[1])
+    m = int(argv[2])
+    result = add(n, m)  
+
+    print(result)
