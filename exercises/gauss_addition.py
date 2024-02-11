1acec245ccd12f9fcde2b342670d483cacd7d6d4
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-11 10:57:13 -0500

    initial commit

---
+++ exercises/gauss_addition.py
@@ -0,0 +1,27 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Gauss Addition: https://edabit.com/challenge/BE2FQ3xwFAC5ZbgPY
+
+Create a function that adds all the numbers together from 1 to n or, if two numbers are given: n to m. The input can be in any order.
+
+Example:
+    Gauss({ 100 }) ➞ 5050 // From the video
+    Gauss({ 5001, 7000 }) ➞ 12001000 // Also ^^
+    Gauss({ 1975, 165 }) ➞ 1937770
+
+Started: Feb 11, 2024 @ 11:00am ET
+Intervals: 1
+Ended: Feb 11, 2024 @ 11:30am ET
+"""
+
+
+
+
+if __name__ == '__main__':
+    pass
