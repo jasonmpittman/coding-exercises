5caed71d7ce895a1796fa388d71b9889ddd1fa94
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-12 04:05:12 -0500

    initial commit

---
+++ exercises/grand_slam.py
@@ -0,0 +1,23 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Grand Sum:
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+Started: Feb 12, 2024 @ 4:05am ET
+Intervals: 1
+Ended: Feb 12, 2024 @ 4:35am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
