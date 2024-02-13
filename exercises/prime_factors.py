ff1a91ea99831751dbd92bd1354c6741f1f5883f
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-13 05:24:55 -0500

    initial commit

---
+++ exercises/prime_factors.py
@@ -0,0 +1,29 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Prime Factors:
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143?
+
+Started: Feb 13, 2024 @ 5:30am ET
+Intervals: 1
+Ended: Feb 13, 2024 @ 6:00am ET
+"""
+from sys import argv
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+
+
