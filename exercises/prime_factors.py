dc5b9411c975a3bac30b490a4ccc64b5870e9ae2
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-13 05:52:42 -0500

    implement find largest prime function

--- exercises/prime_factors.py
+++ exercises/prime_factors.py
@@ -14,16 +14,29 @@ What is the largest prime factor of the number 600851475143?
 
 Started: Feb 13, 2024 @ 5:30am ET
 Intervals: 1
-Ended: Feb 13, 2024 @ 6:00am ET
+Ended: Feb 13, 2024 @ 5:52am ET
 """
 from sys import argv
 
 
-
+def find_largest_prime(n: int) -> int:
+    """Implements a naive division approach"""
+    divisor = 2
+    while divisor < n:
+        if n % divisor == 0 and n / divisor > 1:
+            n = n / divisor
+            divisor = 2
+        else:
+            divisor += 1
+        
+    return int(n)
 
 
 
 if __name__ == '__main__':
-    pass
+    n = int(argv[1])
+
+    largest_prime = find_largest_prime(n)
+    print(largest_prime)
 
 
