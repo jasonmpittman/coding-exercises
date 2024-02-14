75763f06043416f8061655eff958117d08bebd7e
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-14 04:55:06 -0500

    initial commit

---
+++ exercises/odds_evens.py
@@ -0,0 +1,24 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Odds and Evens:
+
+Write a function that takes a list of numbers and returns a list with two elements:
+
+    The first element should be the sum of all even numbers in the list.
+    The second element should be the sum of all odd numbers in the list.
+
+Started: Feb 14, 2024 @ 4:55am ET
+Intervals: 1
+Ended: Feb 14, 2024 @ 5:25am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
