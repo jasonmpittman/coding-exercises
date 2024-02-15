7e81e59fa121f10f065f1fde871cd4a1e45d1152
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-15 05:53:15 -0500

    initial commit

---
+++ exercises/lexicographic_permutation.py
@@ -0,0 +1,26 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Lexicographic Permutation:
+
+A permutation is an ordered arrangement of objects. For exmaple, 3124 is one possible permutation of the digits 1,2,3, and 4. If all of the permutations are listed numerically or alphabetically, we call it lexicographic order. The lexicographic permutations of 0, 1, and 2 are:
+
+012, 021, 102, 120, 201, 210
+
+What is the millionth lexicographic permutation of the digits: 0, 1, 2, 3, 4, 5, 6, 7, 8, and 9?
+
+
+Started: Feb 14, 2024 @ 5:55am ET
+Intervals: 1
+Ended: Feb 14, 2024 @ 6:25am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
