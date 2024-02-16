5845701228d0703f05f143c756965a6e6046fc23
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-16 05:44:34 -0500

    initial commit

---
+++ exercises/pair_sum.py
@@ -0,0 +1,28 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Pair Sum:
+
+Given an unsorted integer array, find a pair with the given sum in it.
+
+Examples:
+    Input:   nums = [8, 7, 2, 5, 3, 1] target = 10  
+    Output:   Pair found (8, 2) or Pair found (7, 3)
+
+    Input:   nums = [5, 2, 6, 8, 1, 9] target = 12  
+    Output: Pair not found 
+
+Started: Feb 16, 2024 @ 5:45am ET
+Intervals: 1
+Ended: Feb 16, 2024 @ 6:15am ET
+"""
+
+
+
+if __name__ == '__main__':
+    pass
