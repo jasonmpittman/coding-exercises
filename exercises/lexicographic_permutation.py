8af836822a3a8aa1a99bdaee7cbff5f3c90864a4
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-16 05:44:20 -0500

    fixed typo in dates

--- exercises/lexicographic_permutation.py
+++ exercises/lexicographic_permutation.py
@@ -15,9 +15,9 @@ A permutation is an ordered arrangement of objects. For exmaple, 3124 is one pos
 What is the millionth lexicographic permutation of the digits: 0, 1, 2, 3, 4, 5, 6, 7, 8, and 9?
 
 
-Started: Feb 14, 2024 @ 5:55am ET
+Started: Feb 15, 2024 @ 5:55am ET
 Intervals: 1
-Ended: Feb 14, 2024 @ 6:25am ET
+Ended: Feb 15, 2024 @ 6:25am ET
 """
 from itertools import permutations
 
