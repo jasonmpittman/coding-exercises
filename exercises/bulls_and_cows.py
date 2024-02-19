b0a6c507e3225a2f9fa724646f638e8d85b1bc84
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-19 05:09:18 -0500

    initial commit

---
+++ exercises/bulls_and_cows.py
@@ -0,0 +1,33 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Bulls and Cows:
+
+Create a four digit random number from the digits 1 to 9, without duplication.
+
+The program should:
+
+1. ask for guesses to this number
+2. reject guesses that are malformed
+3. print the score for the guess
+
+The score is computed as:
+
+    The player wins if the guess is the same as the randomly chosen number, and the program ends.
+    A score of one bull is accumulated for each digit in the guess that equals the corresponding digit in the randomly chosen initial number.
+    A score of one cow is accumulated for each digit in the guess that also appears in the randomly chosen number, but in the wrong position.
+
+Started: Feb 19, 2024 @ 5:10am ET
+Intervals: 1
+Ended: Feb 19, 2024 @ 5:40am ET
+"""
+
+
+
+if __name__ == "__main__":
+    pass
