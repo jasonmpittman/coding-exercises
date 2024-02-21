2df3b2bafb051716876ec3e76e63123c47cdc534
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-21 05:35:14 -0500

    initial commit

---
+++ exercises/factorial_recursion.py
@@ -0,0 +1,21 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Factorial Recursion: 
+
+The recursive function for this challenge should return the factorial of an inputted integer. If anyone needs a refresher, factorials in mathematics are represented by an exclamation point placed to the right of a number: 4! = 4 x 3 x 2 x 1 = 24
+
+Started: Feb 21, 2024 @ 5:35am ET
+Intervals: 1
+Ended: Feb 21, 2024 @ 6:05am ET
+"""
+from sys import argv
+
+
+if __name__ == "__main__":
+    pass
