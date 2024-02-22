1e68b5fe65f80d5127b55061568d9c3c076e9ba2
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-22 04:57:39 -0500

    initial commit

---
+++ exercises/next_largest.py
@@ -0,0 +1,33 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+"""" Next Largest Integer:
+Given an integer, find the next largest integer using ONLY the digits from the given integer.
+
+Examples:
+    Input:
+        1234
+        1243
+        234765
+        19000
+    Output:
+        1243
+        1324
+        235467
+        90001
+
+
+Started: Feb 22, 2024 @ 5:00am ET
+Intervals: 1
+Ended: Feb 22, 2024 @ 5:30am ET
+"""
+
+
+
+if __name__ == "__main__":
+    pass
