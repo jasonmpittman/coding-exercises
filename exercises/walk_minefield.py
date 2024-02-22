78e120e83c6a008fa57b6bafca0b900bb56baa09
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-22 04:55:49 -0500

    suspending because of scope

--- exercises/walk_minefield.py
+++ exercises/walk_minefield.py
@@ -6,7 +6,7 @@ __version__ = "1.0.0"
 __maintainer__ = "Jason M. Pittman"
 __status__ = "Exercise"
 
-""" Walk a Minefield:
+""" Walk a Minefield: https://old.reddit.com/r/dailyprogrammer/comments/7d4yoe/20171114_challenge_340_intermediate_walk_in_a/
 
 You must remotely send a sequence of orders to a robot to get it out of a minefield.
 
@@ -44,7 +44,25 @@ Intervals: 1
 Ended: Feb 22, 2024 @ 5:20am ET
 """
 
-
+def move_robot(current_position, command):
+    pass
 
 if __name__ == "__main__":
-    pass
+    ROBOT_ON = False
+    
+    minefield = [
+        "+++++++++++++",
+        "+000000000000",
+        "+0000000*000+",
+        "+00000000000+",
+        "+00000000*00+",
+        "+00000000000+",
+        "M00000000000+",
+        "+++++++++++++"
+    ]
+
+    x = 6
+    y = 0
+    current_position = [x, y]
+
+
