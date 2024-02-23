cff0d55606dda0bade2395d14d013a533af2684d
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-23 16:00:25 -0500

    experimented with possible solutions

--- exercises/matrix_zeroes.py
+++ exercises/matrix_zeroes.py
@@ -22,7 +22,53 @@ Intervals: 1
 Ended: Feb 23, 2024 @ 4:00pm ET
 """
 
+# TODO: original instinct closer to correct than suggested solution
+def zero_the_matrix(matrix: list) -> None:
+    # if any m or n is 0, 0 both m and n
 
+    """
+    number_of_rows = len(matrix[0])
+    number_of_columns = len(matrix)
+    
+    zeroed_matrix = []
+
+    for i in range(number_of_columns - 1):
+        for j in range(number_of_rows):
+            if matrix[i][j] == 0:
+                zeroed_matrix.append((i, j))
+
+    for row, col in zeroed_matrix:
+        matrix[row] = [0] * number_of_rows
+
+        for i in range(number_of_rows):
+            matrix[i][col] = 0
+
+    print(zeroed_matrix)
+    
+    
+    """
+
+
+    n = len(matrix)
+    m = len(matrix[0])
+
+    V = []
+
+    for i in range(n):
+        for j in range(m):
+            if matrix[i][j] == 0:
+                V.append((i, j))
+
+        for row, col in V:
+            matrix[row] = [0] * m
+
+            for i in range(n):
+                matrix[i][col] = 0    
+
+
+    print(V)
 
 if __name__ == "__main__":
-    pass
+    matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+
+    zero_the_matrix(matrix) # outputs [(0, 0), (0, 3), (1, 0), (1, 3), (2, 0), (2, 3)]
