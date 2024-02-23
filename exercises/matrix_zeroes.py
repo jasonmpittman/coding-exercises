0f229a62e88414b15787a21c71450798204e9a37
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-23 15:29:06 -0500

    initial commit

---
+++ exercises/matrix_zeroes.py
@@ -0,0 +1,28 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Matrix Zeroes:
+
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+Examples:
+    Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+    Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+    Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
+    Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
+
+Started: Feb 23, 2024 @ 3:30pm ET
+Intervals: 1
+Ended: Feb 23, 2024 @ 4:00pm ET
+"""
+
+
+
+if __name__ == "__main__":
+    pass
