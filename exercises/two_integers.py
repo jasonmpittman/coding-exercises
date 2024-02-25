3d45763d2a9586630b23af3491f1319110a7142c
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-25 07:13:43 -0500

    implemented sum two integers function

--- exercises/two_integers.py
+++ exercises/two_integers.py
@@ -21,13 +21,27 @@ Example:
 
 Started: Feb 25, 2024 @ 6:50am ET
 Intervals: 1
-Ended: Feb 25, 2024 @ 7:20am ET
+Ended: Feb 25, 2024 @ 7:15am ET
 """
 from sys import argv
 
+# TODO: need to handle no sum
+def sum_two_integers(length: int, target_sum: int, numbers: list):
+    x, y = 0, 0
+    
+    for i in range(length):
+        y = numbers[i]
+        for j in range(length):
+            x = numbers[j]
+            if x + y == target_sum:
+                break
 
-
+    print("Indices are: ", numbers.index(x) + 1, numbers.index(y) + 1) # +1 to convert to nonzero indexing
 
 
 if __name__ == "__main__":
-    pass
+    length = int(argv[1])
+    target_sum = int(argv[2])
+    numbers = list(map(int, argv[3].split(','))) # 2,7,5,1
+
+    sum_two_integers(length, target_sum, numbers)
