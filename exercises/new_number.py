893ad5d39f31491e5a985f789b0e5e22897b206d
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-24 20:47:25 -0500

    initial commit

---
+++ exercises/new_number.py
@@ -0,0 +1,22 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" New Number:
+A number is input in computer then a new no should get printed by adding one to each of its digit. If you encounter a 9, insert a 10 (don't carry over, just shift things around).
+
+For example, 998 becomes 10109.
+
+Started: Feb 24, 2024 @ 8:50pm ET
+Intervals: 1
+Ended: Feb 24, 2024 @ 9:20pm ET
+"""
+from sys import argv
+
+
+if __name__ == "__main__":
+    pass
