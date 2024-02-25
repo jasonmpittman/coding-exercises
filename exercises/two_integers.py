26fb085011288dc6116542786bdf153dfc093143
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-25 06:50:49 -0500

    initial commit

---
+++ exercises/two_integers.py
@@ -0,0 +1,33 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Two Integers:
+
+You are given an array of n integers, and your task is to find two values (at distinct positions) whose sum is x.
+
+Example:
+
+    Input:
+    4 8
+    2 7 5 1
+
+    Output:
+    2 4
+
+Started: Feb 25, 2024 @ 6:50am ET
+Intervals: 1
+Ended: Feb 25, 2024 @ 7:20am ET
+"""
+from sys import argv
+
+
+
+
+
+if __name__ == "__main__":
+    pass
