884b573faecdfe8ac8720e1f55a7d1c2e688b2d3
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-26 06:35:33 -0500

    implemented bubble sorting function

--- exercises/bubble_sort.py
+++ exercises/bubble_sort.py
@@ -12,11 +12,32 @@ Timed exercise to implement bubble sorting on a list of integers
 
 Started: Feb 26, 2024 @ 6:25am ET
 Intervals: 1
-Ended: Feb 26, 2024 @ 6:55am ET
+Ended: Feb 26, 2024 @ 6:33am ET
 """
 from sys import argv
 
+def bubble_sort(numbers: list) -> list:
+    length = len(numbers)
+    swapped = False
+
+    print("Starting list is: ", numbers)
+
+    for i in range(0, length):
+
+        for j in range(0, length-i-1):
+        
+            if numbers[j] > numbers[j+1]:
+                numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+                print("Changed list is now: ", numbers)
+                swapped = True
+
+        if swapped == False:
+            break
+
+    return numbers
 
 
 if __name__ == "__main__":
-    pass
+    # 2,0,4,1,3
+    numbers = argv[1].split(',')
+    bubble_sort(numbers)
