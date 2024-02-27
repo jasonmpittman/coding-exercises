2921e473ee6deafcb79b67f77b84439dd16b07ea
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-27 04:00:22 -0500

    initial commit

---
+++ exercises/permutation_printing.py
@@ -0,0 +1,33 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Permutation Printing:
+
+You are given a positive integer 𝑛
+
+Find a permutation† 𝑝 of length 𝑛 such that there do not exist two distinct indices 𝑖 and 𝑗 (1≤𝑖,𝑗<𝑛; 𝑖≠𝑗) such that 𝑝𝑖 divides 𝑝𝑗 and 𝑝𝑖+1 divides 𝑝𝑗+1
+
+Example:
+    Input: 
+        2
+        4
+        3
+    
+    Output
+        4 1 2 3
+        1 2 3
+
+Started: Feb 27, 2024 @ 4:00am ET
+Intervals: 1
+Ended: Feb 27, 2024 @ 4:30am ET
+"""
+from sys import argv
+
+
+if __name__ == "__main__":
+    pass
