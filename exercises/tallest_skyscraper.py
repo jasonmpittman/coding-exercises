b555bc650f4bceac295c5b2f7dc5b64f84fd29e0
Jason M. Pittman <jason@jasonmpittman.com>
2024-02-29 04:59:35 -0500

    initial commit

---
+++ exercises/tallest_skyscraper.py
@@ -0,0 +1,32 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Tallest Skyscraper:
+
+A city skyline can be represented as a 2-D list with 1s representing buildings. In the example below, the height of the tallest building is 4 (second-most right column).
+
+Example:
+    [[0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 0],
+    [0, 0, 1, 0, 1, 0],
+    [0, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1]]
+
+    -> 4
+
+Create a function that takes a skyline (2-D list of 0's and 1's) and returns the height of the tallest skyscraper.
+
+Started: Feb 29, 2024 @ 5:00am ET
+Intervals: 1
+Ended: Feb 29, 2024 @ 5:30am ET
+"""
+
+
+
+if __name__ == "__main__":
+    pass
