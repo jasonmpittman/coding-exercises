f64b554f7cb888aed9a4d6bb7d37f1738dd96d06
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-02 05:06:19 -0500

    initial commit

---
+++ exercises/array_depth.py
@@ -0,0 +1,30 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Array Depth:
+Given an array, write a function to calculate it's depth. Assume that a normal array has a depth of 1.
+
+Examples:
+    depth([1, 2, 3, 4]) ➞ 1
+
+    depth([1, [2, 3, 4]]) ➞ 2
+
+    depth([1, [2, [3, 4]]]) ➞ 3
+
+    depth([1, [2, [3, [4]]]]) ➞ 4
+
+Started: Mar 02, 2024 @ 5:10am ET
+Intervals: 1
+Ended: Marc 02, 2024 @ 5:40am ET
+"""
+
+
+
+
+if __name__ == "__main__":
+    pass
