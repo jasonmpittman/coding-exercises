6d0b60cf437b3ca3f8be8a7316bdc7bdd998919f
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-02 05:43:33 -0500

    implemented two different functions to count depth

--- exercises/array_depth.py
+++ exercises/array_depth.py
@@ -22,9 +22,31 @@ Started: Mar 02, 2024 @ 5:10am ET
 Intervals: 1
 Ended: Marc 02, 2024 @ 5:40am ET
 """
+from collections import Counter
 
+# this works but only with the +1 after the return
+def calculate_depth(array: list) -> int:  
+    
+    depth = sum([1 + calculate_depth(i) for i in array if isinstance(i, list)])
 
+    return depth
 
+# TODO: this is off
+def compute_depth(array: list) -> int:
+    depth = Counter(map(len, array))
+    s = 0
+    print(depth)
 
+    for k,v in depth.items():
+        s += int(k) # do we want k or v here?
+    
+    print(s)
+
+# this only works with string lists because of compute_depth
 if __name__ == "__main__":
-    pass
+    array = ['1', ['2', '3', '4']] #['1', ['2', ['3', ['4']]]] #['1', ['2', '3', '4']] #[1, 2, 3, 4]
+
+    depth = calculate_depth(array)
+    print(depth + 1)
+
+    compute_depth(array)
