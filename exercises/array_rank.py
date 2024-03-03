2302f09f4ab8d0927548aa02159fe53aed64a349
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-03 04:32:19 -0500

    initial commit

---
+++ exercises/array_rank.py
@@ -0,0 +1,27 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Array Rank:
+
+Given an array of distinct integers, replace each array element by its corresponding rank in the array.
+
+The minimum array element has the rank 1; the second minimum element has a rank of 2, and so on…
+
+Example:
+    Input:  { 10, 8, 15, 12, 6, 20, 1 }
+    Output: { 4, 3, 6, 5, 2, 7, 1 } 
+
+Started: Mar 03, 2024 @ 4:35am ET
+Intervals: 1
+Ended: Marc 03, 2024 @ 5:05am ET
+"""
+
+
+
+if __name__ == "__main__":
+    pass
