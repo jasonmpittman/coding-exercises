414694ba56111ca2765d7091af7431fe7015ca1e
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-04 06:56:01 -0500

    initial commit

---
+++ exercises/karaca_encryption.py
@@ -0,0 +1,41 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" The Karaca's Encryption Algorithm:
+
+Make a function that encrypts a given input with these steps:
+
+Input: "apple"
+
+Step 1: Reverse the input: "elppa"
+
+Step 2: Replace all vowels using the following chart:
+
+a => 0
+e => 1
+i => 2
+o => 2
+u => 3
+
+# "1lpp0"
+
+Step 3: Add "aca" to the end of the word: "1lpp0aca"
+
+Output: "1lpp0aca"
+
+Started: Mar 04, 2024 @ 7:00am ET
+Intervals: 1
+Ended: Marc 04, 2024 @ 7:30am ET
+"""
+
+
+
+
+
+if __name__ == "__main__":
+    pass
