7a388bbdecdbb804ddcd1e7c23ac13d39dd08504
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-05 04:05:36 -0500

    implemented sort by insertion function

--- exercises/insertion_sort.py
+++ exercises/insertion_sort.py
@@ -19,14 +19,34 @@ Output: [-8, -6, -3, -2, 5, 9]
 
 Started: Mar 05, 2024 @ 3:50am ET
 Intervals: 1
-Ended: Marc 05, 2024 @ 4:20am ET
+Ended: Marc 05, 2024 @ 4:05am ET
 """
 from sys import argv
 
 
+def sort_by_insertion(array: list) -> list:
+    length = len(array)
+
+    if length == 0 or length == 1:
+        return array
+
+    for i in range(1, length):
+        current_element = array[i]
+        selected_element_index = i - 1
+        print("Current element is {} and selected index is {}".format(current_element, selected_element_index))
+
+        while selected_element_index >= 0 and current_element < array[selected_element_index]:
+            array[selected_element_index + 1] = array[selected_element_index]
+            selected_element_index -= 1
+        
+        array[selected_element_index + 1] = current_element
+
+    return array
+        
 
 
-
-if __name__ == "__main__":
-    pass
-
+if __name__ == "__main__": # 6,3,4,8,2,9 # 9,-3,5,-2,-8,-6
+    array = list(map(int, argv[1].split(',')))
+    
+    sorted_array = sort_by_insertion(array)
+    print(sorted_array)
