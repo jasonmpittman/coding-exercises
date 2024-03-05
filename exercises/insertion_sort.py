114c00c5fdcdebf3f0af6df55f3e262b8100a96a
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-05 03:40:09 -0500

    initial commit

---
+++ exercises/insertion_sort.py
@@ -0,0 +1,32 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Insertion Sort:
+
+Given an integer array, in-place sort it without using any inbuilt functions.
+
+Input : [6, 3, 4, 8, 2, 9]
+Output: [2, 3, 4, 6, 8, 9]
+
+Input : [9, -3, 5, -2, -8, -6]
+Output: [-8, -6, -3, -2, 5, 9]
+
+
+Started: Mar 05, 2024 @ 3:50am ET
+Intervals: 1
+Ended: Marc 05, 2024 @ 4:20am ET
+"""
+from sys import argv
+
+
+
+
+
+if __name__ == "__main__":
+    pass
+
