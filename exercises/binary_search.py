b8002deab7e19e04ea0bd0862ab08601c4fc35ff
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-07 05:55:54 -0500

    initial commit

---
+++ exercises/binary_search.py
@@ -0,0 +1,29 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Binary Search:
+Given a sorted array of n integers and a target value, determine if the target exists in the array in logarithmic time using the binary search algorithm. If target exists in the array, print the index of it.
+
+Examples:
+    Input:   nums[] = [2, 3, 5, 7, 9] target = 7  
+    Output: Element found at index 3    
+
+    Input:   nums[] = [1, 4, 5, 8, 9] target = 2  
+    Output: Element not found
+
+Started: Mar 07, 2024 @ 6:00am ET
+Intervals: 1
+Ended: Marc 07, 2024 @ 6:30am ET
+"""
+from sys import argv
+
+
+
+
+if __name__ == "__main__":
+    pass
