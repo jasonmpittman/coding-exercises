24f868b8dcc8a5b6b27244d16d1d244cd1eba272
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-08 05:10:47 -0500

    initial commit

---
+++ exercises/unique_sort.py
@@ -0,0 +1,33 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Unique Sort: 
+Given an array of numbers, write a function that returns an array that...
+
+    Has all duplicate elements removed.
+    Is sorted from least to greatest value.
+
+Examples:
+    uniqueSort([1, 2, 4, 3]) ➞ [1, 2, 3, 4]
+    uniqueSort([1, 4, 4, 4, 4, 4, 3, 2, 1, 2]) ➞ [1, 2, 3, 4]
+    uniqueSort([6, 7, 3, 2, 1]) ➞ [1, 2, 3, 6, 7]   
+
+
+Started: Mar 08, 2024 @ 5:10am ET
+Intervals: 1
+Ended: Marc 08, 2024 @ 5:40am ET
+"""
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
