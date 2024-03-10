b23df5fae1614b37b28b6f6077fb5bbce18b6237
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-10 06:50:17 -0400

    initial commit

---
+++ exercises/integer_in_range.py
@@ -0,0 +1,29 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Integer in Range:
+Create a function that validates whether a number n is within the bounds of lower and upper. Return false if n is not an integer.
+
+Examples:
+    intWithinBounds(3, 1, 9) ➞ true
+    intWithinBounds(6, 1, 6) ➞ false
+    intWithinBounds(4.5, 3, 8) ➞ false
+
+Started: Mar 10, 2024 @ 6:50pm ET
+Intervals: 1
+Ended: March 10, 2024 @ 7:20pm ET
+"""
+from sys import argv
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
