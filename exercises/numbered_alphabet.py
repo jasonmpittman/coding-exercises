7d4b4757f5a90249bb87871d8bb3910b557ef49b
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-11 06:08:50 -0400

    implemented number alphabet function

--- exercises/numbered_alphabet.py
+++ exercises/numbered_alphabet.py
@@ -16,8 +16,25 @@ Examples:
 
 Started: Mar 11, 2024 @ 6:00pm ET
 Intervals: 1
-Ended: March 11, 2024 @ 6:30pm ET
+Ended: March 11, 2024 @ 6:07pm ET
 """
 from sys import argv
 
 
+def number_alphabet(plaintext: str) -> list:
+    ciphertext = ""
+    
+    # didn't feel like hardcoding a dictionary
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet = {letter:str(num) for (num,letter) in enumerate(letters.upper()) }
+
+    for char in plaintext:
+        ciphertext += alphabet[char] + ' '
+
+    return ciphertext
+
+if __name__ == "__main__":
+    plaintext = argv[1].upper()
+
+    ciphertext = number_alphabet(plaintext)
+    print(ciphertext)
