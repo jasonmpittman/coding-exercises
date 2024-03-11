47ade5253378eff775fd0454c4138ff66a34f872
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-11 06:01:15 -0400

    initial commit

---
+++ exercises/numbered_alphabet.py
@@ -0,0 +1,23 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Numbered Alphabet:
+Create a function that converts a string of letters to their respective number in the alphabet.
+
+Examples:
+    alphNum("XYZ") ➞ "23 24 25"
+    alphNum("ABCDEF") ➞ "0 1 2 3 4 5"
+    alphNum("JAVASCRIPT") ➞ "9 0 21 0 18 2 17 8 15 19"
+
+Started: Mar 11, 2024 @ 6:00pm ET
+Intervals: 1
+Ended: March 11, 2024 @ 6:30pm ET
+"""
+from sys import argv
+
+
