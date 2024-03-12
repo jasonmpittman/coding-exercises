ef0e37007627404496fc7f119100e41de3ecda65
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-12 05:23:48 -0400

    initial commit

---
+++ exercises/filter_primes.py
@@ -0,0 +1,31 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2023"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Filter Primes:
+Create a function that takes an array and returns a new array containing only prime numbers.
+
+Examples
+
+    filterPrimes([7, 9, 3, 9, 10, 11, 27]) ➞ [7, 3, 11]
+
+    filterPrimes([10007, 1009, 1007, 27, 147, 77, 1001, 70]) ➞ [10007, 1009]
+
+    filterPrimes([1009, 10, 10, 10, 3, 33, 9, 4, 1, 61, 63, 69, 1087, 1091, 1093, 1097]) ➞ [1009, 3, 61, 1087, 1091, 1093, 1097]
+
+Started: Mar 12, 2024 @ 5:25pm ET
+Intervals: 1
+Ended: March 12, 2024 @ 5:55pm ET
+"""
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
