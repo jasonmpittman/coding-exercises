1fd17ba319242bddfc8bf62342cfadde7697738b
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-14 05:29:17 -0400

    initial commit

---
+++ exercises/moon_sum.py
@@ -0,0 +1,45 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Moon Sum:
+When two numbers are added together, the strange Lunar arithmetic is used on the Moon. The Lunar sum of two numbers is not determined by the sum of their individual digits, but by the highest digit of the two taken into account at each step, in columnar form.
+
+2  4  6  +
+3  1  7  =
+--------
+3  4  7
+
+// 3 > 2 | 4 > 1 | 7 > 6
+
+1  3  4  +
+   5  4  =
+--------
+1  5  4
+
+//  1 > 0 | 5 > 3 | 4 == 4
+// Blank spots in the columnar form are equals to 0
+
+   2  0  +
+1  4  0  =
+-------
+1  4  0
+
+// 1 > 0 | 4 > 2 | 0 == 0
+
+Started: Mar 14, 2024 @ 5:30am ET
+Intervals: 1
+Ended: March 14, 2024 @ 6:00am ET
+"""
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
