8deca58e7bd648f7173dc555cae83ed2be68eddc
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-15 05:12:17 -0400

    initial commit

---
+++ exercises/alternate_binary.py
@@ -0,0 +1,33 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Alternating Binary:
+Write a function that returns true if the binary string can be rearranged to form a string of alternating 0s and 1s.
+
+Examples
+
+    canAlternate("0001111") ➞ true
+    // Can make: "1010101"
+    canAlternate("01001") ➞ true
+    // Can make: "01010"
+    canAlternate("010001") ➞ false
+    canAlternate("1111") ➞ false
+
+Started: Mar 15, 2024 @ 5:15am ET
+Intervals: 1
+Ended: March 15, 2024 @ 5:45am ET
+"""
+from sys import argv
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
