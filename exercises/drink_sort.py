6f76139d65c3b938f7a3f3fc5333d2ce3ba1e0bb
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-16 05:39:04 -0400

    initial commit

---
+++ exercises/drink_sort.py
@@ -0,0 +1,21 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Drink Sort:
+You will be given an array of drinks, with each drink being an object with two properties: name and price. Create a function that has the drinks array as an argument and return the drinks objects sorted by price in ascending order.
+
+Started: Mar 16, 2024 @ 5:40am ET
+Intervals: 1
+Ended: March 16, 2024 @ 6:10am ET
+"""
+import json
+
+
+
+if __name__ == "__main__":
+    pass
