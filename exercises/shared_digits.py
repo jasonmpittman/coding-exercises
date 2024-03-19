9ddf6417a5871695f239fbf364bdb9df03a31ab0
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-19 06:26:24 -0400

    implemented shared digits function

--- exercises/shared_digits.py
+++ exercises/shared_digits.py
@@ -19,5 +19,39 @@ Examples
 
 Started: Mar 18, 2024 @ 6:00am ET
 Intervals: 1
-Ended: March 18, 2024 @ 6:30am ET
+Ended: March 18, 2024 @ 6:25am ET
 """
+
+from sys import argv
+
+
+def is_number_shared(n: str, m: str) -> bool:
+    shared = False
+
+    for i in [*n]:
+        shared = True if i in [*m] else False
+        if shared == True:
+            break
+
+    return shared
+
+def are_digits_shared(array: list) -> bool:
+    l = len(array)
+    shared = False
+    i = 0
+    j = 1
+
+    while j < l:
+        print("Comparing {} and {}".format(array[i], array[j]))
+        shared = is_number_shared(array[i], array[j])
+        
+        i += 1
+        j += 1
+    
+    return shared
+
+    
+
+if __name__ == "__main__":
+    array = argv[1].split(',') # 1,11,12,13,14,15,16  33,44,55,66,77  33,53,6351,12,2242,44
+    print(are_digits_shared(array))
