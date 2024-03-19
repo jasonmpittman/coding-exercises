093674e25a3b82b25474e3b814650d13d61b4997
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-19 06:00:33 -0400

    initial commit

---
+++ exercises/shared_digits.py
@@ -0,0 +1,23 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Shared Digits:
+Create a function that returns true if each pair of adjacent numbers in an array shares at least one digit and false otherwise.
+
+Examples
+
+    sharedDigits([33, 53, 6351, 12, 2242, 44]) ➞ true
+    // 33 and 53 share 3, 53 and 6351 share 3 and 5, etc.
+    sharedDigits([1, 11, 12, 13, 14, 15, 16]) ➞ true
+    sharedDigits([33, 44, 55, 66, 77]) ➞ false
+    sharedDigits([1, 12, 123, 1234, 1235, 6789]) ➞ false
+
+Started: Mar 18, 2024 @ 6:00am ET
+Intervals: 1
+Ended: March 18, 2024 @ 6:30am ET
+"""
