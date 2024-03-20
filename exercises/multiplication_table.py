ecbe7aca6bb57a968e95b226e609fbc9427ec9b4
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-20 05:33:39 -0400

    initial commit

---
+++ exercises/multiplication_table.py
@@ -0,0 +1,31 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Multiplication Table:
+Your task, is to create N x N multiplication table, of size n provided in parameter.
+
+For example, when n is 5, the multiplication table is:
+
+    1, 2, 3, 4, 5
+    2, 4, 6, 8, 10
+    3, 6, 9, 12, 15
+    4, 8, 12, 16, 20
+    5, 10, 15, 20, 25
+
+    [[1, 2, 3, 4, 5], [2, 4, 6, 8, 10], [3, 6, 9, 12, 15], [4, 8, 12, 16, 20], [5, 10, 15, 20, 25]]
+
+Examples:
+
+    multiplicationTable(1) ➞ [[1]]
+    multiplicationTable(3) ➞ [[1, 2, 3], [2, 4, 6], [3, 6, 9]]
+
+
+Started: Mar 20, 2024 @ 5:35am ET
+Intervals: 1
+Ended: March 20, 2024 @ 6:05am ET
+"""
