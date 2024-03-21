643c744b14371d2adafa4ce4d8365fc292aa21b1
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-21 05:42:33 -0400

    initial commit

---
+++ exercises/word_salad.py
@@ -0,0 +1,20 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Word Salad:
+Slice each word in half and sort the chunks alphabetically. This recipe tastes best when the chunks are joined together to make a string.
+
+Examples:
+    fruitSalad(["apple", "pear", "grapes"]) ➞ "apargrapepesple"
+    fruitSalad(["raspberries", "mango"]) ➞ "erriesmangoraspb"
+    fruitSalad(["banana"]) ➞ "anaban"
+
+Started: Mar 21, 2024 @ 5:45am ET
+Intervals: 1
+Ended: March 21, 2024 @ 6:15am ET
+"""
