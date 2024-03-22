88d2dae86c4f3d70325abba7a06fd9b0ccee87a1
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-22 05:57:33 -0400

    initial commit

---
+++ exercises/ranged_reversal.py
@@ -0,0 +1,20 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Ranged Reversal:
+Write a function that reverses the subarray between the start and end index (inclusive). The rest of the array should be kept the same.
+
+Examples:
+    rangedReversal([1, 2, 3, 4, 5, 6], 1, 3) ➞ [1, 4, 3, 2, 5, 6]
+    rangedReversal([1, 2, 3, 4, 5, 6], 0, 4) ➞ [5, 4, 3, 2, 1, 6]
+    rangedReversal([9, 8, 7, 4], 0, 0) ➞ [9, 8, 7, 4]
+
+Started: Mar 22, 2024 @ 6:00am ET
+Intervals: 1
+Ended: March 22, 2024 @ 6:30am ET
+"""
