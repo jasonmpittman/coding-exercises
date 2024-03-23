60274862c876d495334dbfc513b2aeea2bf760d1
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-23 07:09:05 -0400

    implemented factorization function

--- exercises/factor_tractor.py
+++ exercises/factor_tractor.py
@@ -16,5 +16,57 @@ Examples:
 
 Started: Mar 23, 2024 @ 6:45am ET
 Intervals: 1
-Ended: March 23, 2024 @ 7:15am ET
+Ended: March 23, 2024 @ 7:08am ET
 """
+from sys import argv
+
+
+def is_prime(n: int) -> bool:
+    """ 
+    Test if n a prime number
+
+    Args: 
+        n (int): the integer to test for primality
+    
+    Return:
+        bool: True if n is a prime and False if n is not a prime
+    """
+    if n > 1:
+        for i in range(2, (n // 2)):
+            if (n % i) == 0:
+                return False
+                break
+        else:
+            return True
+    else:
+        return False
+
+def factorize(n: int) -> list:
+    factors = []
+
+    i = 2
+    while n > 1:
+        if n % i == 0:
+            factors.append(i)
+            n = n / i
+            i = i - 1
+        i += 1      
+
+    return factors
+
+    # using sieve of Erastothenes
+    """if n <= 2:
+        return []
+    sieve = [True]*(n + 1)
+
+    for x in range(3, int(n**0.5) + 1, 2):
+        for y in range(3, (n // x) + 1, 2):
+            sieve[(x * y)] = False
+
+    return [2] + [i for i in range(3, n, 2) if sieve[i]]"""
+
+if __name__ == "__main__":
+    n = int(argv[1])
+    
+    factors = factorize(n)
+    print(factors)
