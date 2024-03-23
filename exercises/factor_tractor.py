c021d3e4a7cc66c79251eab1403380cc0fa00f57
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-23 06:42:33 -0400

    initial commit

---
+++ exercises/factor_tractor.py
@@ -0,0 +1,20 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Factor Tractor:
+Write a program to find all the prime factors of a given number. The program must return an array containing all the prime factors, sorted in ascending order. Remember that 1 is neither prime nor composite and should not be included in your output array.
+
+Examples:
+    primeFactorize(25) ➞ [5, 5]
+    primeFactorize(19) ➞ [19]
+    primeFactorize(77) ➞ [7, 11]
+
+Started: Mar 23, 2024 @ 6:45am ET
+Intervals: 1
+Ended: March 23, 2024 @ 7:15am ET
+"""
