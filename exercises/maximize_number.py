98829005dc2a669343d741ab17b95b94840866b4
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-25 04:36:38 -0400

    initial commit

---
+++ exercises/maximize_number.py
@@ -0,0 +1,27 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Maximize Number:
+Write a function that makes the first number as large as possible by swapping out its digits for digits in the second number.
+
+Examples:
+
+    maxPossible(9328, 456) ➞ 9658
+    // 9658 is the largest possible number built from swaps from 456.
+    // 3 replaced with 6 and 2 replaced with 5.
+
+    maxPossible(523, 76) ➞ 763
+    maxPossible(9132, 5564) ➞ 9655
+    maxPossible(8732, 91255) ➞ 9755
+
+Started: Mar 25, 2024 @ 4:45am ET
+Intervals: 1
+Ended: March 25, 2024 @ 5:15am ET
+"""
+
+
