b8dcb8f557dcd7052eff68ffae2213908f4f5392
Jason M. Pittman <jason@jasonmpittman.com>
2024-03-30 05:40:31 -0400

    initial commit

---
+++ exercises/envelopes.py
@@ -0,0 +1,30 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Envelopes:
+You are given a 2D array of integers envelopes where envelopes[i] = [wi, hi] represents the width and the height of an envelope.
+
+One envelope can fit into another if and only if both the width and height of one envelope are greater than the other envelope's width and height.
+
+Return the maximum number of envelopes you can Russian doll (i.e., put one inside the other).
+
+Note: You cannot rotate an envelope.
+
+Examples:
+
+Input: envelopes = [[5,4],[6,4],[6,7],[2,3]]
+Output: 3
+Explanation: The maximum number of envelopes you can Russian doll is 3 ([2,3] => [5,4] => [6,7]).
+
+Input: envelopes = [[1,1],[1,1],[1,1]]
+Output: 1
+
+Started: Mar 30, 2024 @ 5:45am ET
+Intervals: 1
+Ended: March 30, 2024 @ 6:15am ET
+"""
