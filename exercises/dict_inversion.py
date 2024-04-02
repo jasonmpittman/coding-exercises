7b1b29bf0727b29f55364483109ee6adf96abb3b
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-02 03:57:19 -0400

    initial commit

---
+++ exercises/dict_inversion.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Dictionary Inversion:
+Write a function that inverts the keys and values of an object.
+
+Examples
+invert({ "z": "q", "w": "f" })
+➞ { "q": "z", "f": "w" }
+
+invert({ "a": 1, "b": 2, "c": 3 })
+➞ { 1: "a", 2: "b", 3: "c" }
+
+invert({ "zebra": "koala", "horse": "camel" })
+➞ { "koala": "zebra", "camel": "horse" }
+
+Started: April 02, 2024 @ 3:55am ET
+Intervals: 1
+Ended: April 02, 2024 @ 4:25am ET
+"""
