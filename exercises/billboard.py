5b1819351cde9ae61d1ea152a60398ac07dfac4e
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-03 05:06:03 -0400

    initial commit

---
+++ exercises/billboard.py
@@ -0,0 +1,22 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Billboard:
+A billboard is an m * n array, where each array element consists of either one letter or a blank space. You are given a phrase and the billboard dimensions. Create a function that determines whether you can place the complete phrase on the billboard.
+
+There are two rules:
+
+    If there is a space between two words:
+        If they are on the same row, you must put a space.
+        If they are two different rows, the space is optional.
+    You can only put COMPLETE words on a row.
+
+Started: April 03, 2024 @ 5:05am ET
+Intervals: 1
+Ended: April 03, 2024 @ 5:35am ET
+"""
