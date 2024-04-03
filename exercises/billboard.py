0c39934bae33581b0364bbe0013acd585b8d4c83
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-03 05:28:28 -0400

    implemented check billboard function

--- exercises/billboard.py
+++ exercises/billboard.py
@@ -18,5 +18,30 @@ There are two rules:
 
 Started: April 03, 2024 @ 5:05am ET
 Intervals: 1
-Ended: April 03, 2024 @ 5:35am ET
+Ended: April 03, 2024 @ 5:28am ET
 """
+from sys import argv
+
+
+def check_billboard(string: str, rows: int, columns: int) -> bool:
+    string_size = len(string)
+
+    # Check if the entire string can fit on a single row
+    if rows == 1 and string_size <= columns:
+        return True
+
+    # Check if the string can fit on multiple rows
+    if ' ' in string and rows >= 2:
+        strings = string.split(' ')
+        if len(strings[0]) <= columns and len(strings[1]) <= columns:
+            return True
+
+    return False
+
+if __name__ == "__main__":
+    string = argv[1]
+    rows = int(argv[2])
+    columns = int(argv[3])
+
+    is_fit = check_billboard(string, rows, columns)
+    print(is_fit)
