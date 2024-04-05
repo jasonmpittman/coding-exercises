2b856e1d148072ae46f7735f4b2300530704d57a
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-05 07:02:39 -0400

    initial commit

---
+++ exercises/winner.py
@@ -0,0 +1,27 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Winner:
+
+Input
+
+The first line of the input contains a single integer n (1≤n≤100000) — the number of games played.
+
+The second line contains a string s, consisting of n uppercase English letters 'A' and 'D' — the outcome of each of the games. The i-th character of the string is equal to 'A' if the Anton won the i-th game and 'D' if Danik won the i-th game.
+Output
+
+If Anton won more games than Danik, print "Anton" (without quotes) in the only line of the output.
+
+If Danik won more games than Anton, print "Danik" (without quotes) in the only line of the output.
+
+If Anton and Danik won the same number of games, print "Friendship" (without quotes).
+
+Started: April 05, 2024 @ 7:05am ET
+Intervals: 1
+Ended: April 05, 2024 @ 7:35am ET
+"""
