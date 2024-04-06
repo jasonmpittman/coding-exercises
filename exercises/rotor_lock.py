8b7583caafb0b272fc35d0507a7523860202449b
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-06 05:53:07 -0400

    initial commit

---
+++ exercises/rotor_lock.py
@@ -0,0 +1,19 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Rotor Lock: https://codeforces.com/problemset/problem/1703/C
+
+
+Started: April 06, 2024 @ 5:55am ET
+Intervals: 1
+Ended: April 06, 2024 @ 6:25am ET
+"""
+from sys import argv
+
+
+
