2b254617b5bca7008e43697e017bb768525373f3
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-08 07:26:01 -0400

    initial commit

---
+++ exercises/prime_extractor.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Prime Extractor:
+Create a function that takes an integer argument and returns an array of prime numbers found in the decimal representation of that number (not factors).
+
+For example, extractPrimes(1717) returns [7, 7, 17, 17, 71].
+
+The array should be in ascending order. If a prime number appears more than once, every occurrence should be listed. If no prime numbers are found, return an empty array.
+
+Examples
+    extractPrimes(1) ➞ []
+    extractPrimes(7) ➞ [7]
+    extractPrimes(73) ➞ [3, 7, 73]
+    extractPrimes(1313) ➞ [3, 3, 13, 13, 31, 131, 313]
+
+Started: April 08, 2024 @ 7:25am ET
+Intervals: 1
+Ended: April 08, 2024 @ 7:55am ET
+"""
