818b51c46a8d02047789729edf3d208a6f778514
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-08 07:41:21 -0400

    implemented prime extractor functions

--- exercises/prime_extractor.py
+++ exercises/prime_extractor.py
@@ -21,5 +21,50 @@ Examples
 
 Started: April 08, 2024 @ 7:25am ET
 Intervals: 1
-Ended: April 08, 2024 @ 7:55am ET
+Ended: April 08, 2024 @ 7:41am ET
 """
+from sys import argv
+
+# test any integer for primality
+def is_prime(n: int) -> bool:
+    if n > 1:
+        # Iterate from 2 to n // 2
+        for i in range(2, (n//2)+1):
+            # If num is divisible by any number between
+            # 2 and n / 2, it is not prime
+            if (n % i) == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+# generate list of substrings from given string
+def get_substrings(string: str) -> list:
+    substrings = []
+
+    for i in range(len(string)):
+        for j in range(i+1, len(string)+1):
+            substrings.append(string[i:j])
+
+    return substrings
+
+def extract_primes(string: str) -> list:
+    primes = []
+
+    # get substrings of string
+    substrings = get_substrings(string)
+
+    # test substrings for primality
+    for s in substrings:
+        if is_prime(int(s)):
+            primes.append(int(s))
+
+    return primes
+
+
+if __name__ == "__main__":
+    string = argv[1]
+
+    primes = extract_primes(string)
+    print(sorted(primes))
