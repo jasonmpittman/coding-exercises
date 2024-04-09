9b52722ca6785fa33cd50eacc4a899cf618a202a
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-09 05:42:00 -0400

    implemented remove nth code

--- exercises/remove_nth.py
+++ exercises/remove_nth.py
@@ -7,11 +7,11 @@ __maintainer__ = "Jason M. Pittman"
 __status__ = "Exercise"
 
 """ Remove Nth:
-Given the head of a linked list, remove the nth node from the end of the list and return its head.
+Given the head of a linked list, remove the nth node from the list and return its head.
 
     Example:
     Input: head = [1,2,3,4,5], n = 2
-    Output: [1,2,3,5]
+    Output: [1,2,4,5]
 
     Input: head = [1], n = 1
     Output: []
@@ -23,3 +23,25 @@ Started: April 09, 2024 @ 5:10am ET
 Intervals: 1
 Ended: April 09, 2024 @ 5:40am ET
 """
+from sys import argv
+import LinkedList
+from collections import deque
+
+if __name__ == "__main__":
+    if argv[1] == 'default':
+        array = argv[2].split(',')
+        n = int(argv[3])
+
+        llist = LinkedList.LinkedList()
+        for a in array:
+            llist.insertAtEnd(a)
+        
+        llist.printLL()
+        llist.remove_at_index(n)
+        llist.printLL()
+    # TODO: del works but not with 0 indexing
+    elif argv[1] == 'deque':
+        llist = deque(argv[2])
+        n = int(argv[3])
+        del llist[n]
+        print(llist)
