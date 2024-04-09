df72d4fe3fe8f6c776d607dec2209d7e940b6288
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-09 05:11:00 -0400

    initial commit

---
+++ exercises/remove_nth.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Remove Nth:
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+    Example:
+    Input: head = [1,2,3,4,5], n = 2
+    Output: [1,2,3,5]
+
+    Input: head = [1], n = 1
+    Output: []
+
+    Input: head = [1,2], n = 1
+    Output: [1]
+
+Started: April 09, 2024 @ 5:10am ET
+Intervals: 1
+Ended: April 09, 2024 @ 5:40am ET
+"""
