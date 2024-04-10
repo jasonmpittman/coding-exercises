255dd99445f8c7719caf90819f065bb7dc912c5e
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-10 07:24:52 -0400

    initial commit

---
+++ exercises/narcissistic_number.py
@@ -0,0 +1,23 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Narcissistic Number:
+A Narcissistic Number is a number that is the sum of its own digits each raised to the power of the number of digits.
+
+For example, take 153 (3 digits), which is narcisstic:
+    1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+    
+    1652 (4 digits), is non-narcisstic:
+    1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
+ 
+Create a function that returns true or false depending upon whether the given number n is a Narcissistic number or not.
+
+Started: April 10, 2024 @ 7:25am ET
+Intervals: 1
+Ended: April 10, 2024 @ 7:55am ET
+"""
