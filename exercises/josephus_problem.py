b32320199066cda40d127a5c4967e50746f2235d
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-12 06:27:45 -0400

    initial commit

---
+++ exercises/josephus_problem.py
@@ -0,0 +1,28 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Josephus Problem:
+Consider a game where there are n children (numbered 1,2,\dots,n) in a circle. During the game, every other child is removed from the circle until there are no children left. In which order will the children be removed?
+
+Input:
+The only input line has an integer n.
+
+Output
+Print n integers: the removal order.
+
+Examples:
+    Input:
+    7
+
+    Output:
+    2 4 6 1 5 3 7
+
+Started: April 12, 2024 @ 6:30am ET
+Intervals: 1
+Ended: April 12, 2024 @ 7:00am ET
+"""
