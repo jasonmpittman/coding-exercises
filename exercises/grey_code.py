c299f3256dcdf7542c824b6916478840d972516a
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-13 05:01:16 -0400

    initial commit

---
+++ exercises/grey_code.py
@@ -0,0 +1,33 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+
+""" Grey Code:
+A Gray code is a list of all 2^n bit strings of length n, where any two successive strings differ in exactly one bit (i.e., their Hamming distance is one). The task is to create a Gray code for a given length n.
+
+Input
+The only input line has an integer n.
+
+Output
+Print 2^n lines that describe the Gray code. You can print any valid solution.
+
+
+Example
+    Input:
+    2
+
+    Output:
+    00
+    01
+    11
+    10
+
+Started: April 13, 2024 @ 5:00am ET
+Intervals: 1
+Ended: April 13, 2024 @ 5:30am ET
+"""
