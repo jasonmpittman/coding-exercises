538e271e7161579e1b8149ca62b413edf1df4042
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-15 14:47:55 -0400

    initial commit

---
+++ exercises/longest_zeroes.py
@@ -0,0 +1,21 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Longest Zeroes:
+Write a function that returns the longest sequence of consecutive zeroes in a binary string.
+
+Examples
+    longestZero("01100001011000") ➞ "0000"
+    longestZero("100100100") ➞ "00"
+    longestZero("11111") ➞ ""
+
+Started: April 15, 2024 @ 2:50pm ET
+Intervals: 1
+Ended: April 13, 2024 @ 3:20pm ET
+"""
+
