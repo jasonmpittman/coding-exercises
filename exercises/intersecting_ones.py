e439000dcb9bceb46faa56b1004d0e0fb77dd95b
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-16 05:26:25 -0400

    initial commit

---
+++ exercises/intersecting_ones.py
@@ -0,0 +1,32 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Intersecting Ones:
+Write a function that returns true if a 2D matrix is a no-intersecting ones matrix and false otherwise. A no-intersecting ones matrix is one where no two ones exist on the same row or column.
+
+Examples:
+    noIntersectingOnes([
+    [0, 1],
+    [1, 0]
+    ]) ➞ true
+
+    noIntersectingOnes([
+    [1, 1],
+    [0, 0]
+    ]) ➞ false
+
+    noIntersectingOnes([
+    [0, 0, 0, 1],
+    [1, 0, 0, 0],
+    [0, 1, 0, 0]
+    ]) ➞ true
+
+Started: April 16, 2024 @ 5:25am ET
+Intervals: 1
+Ended: April 16, 2024 @ 5:55am ET
+"""
