2fb58868c330ab99d62ab1477b65b38b9d4fbe89
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-17 05:41:28 -0400

    initial commit

---
+++ exercises/array_to_set.py
@@ -0,0 +1,21 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Array to Set: 
+Create a function that sorts a list and removes all duplicate items from it. Output list.
+
+Examples:
+    set([1, 3, 3, 5, 5]) ➞ [1, 3, 5]
+    set([4, 4, 4, 4]) ➞ [4]
+    set([5, 7, 8, 9, 10, 15]) ➞ [5, 7, 8, 9, 10, 15]
+    set([3, 3, 3, 2, 1]) ➞ [1, 2, 3]
+
+Started: April 17, 2024 @ 5:40am ET
+Intervals: 1
+Ended: April 17, 2024 @ 6:10am ET
+"""
