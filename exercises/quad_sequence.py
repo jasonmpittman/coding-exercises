2a924242925eb806d3d9538955bdc7adc758d9c6
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-19 07:10:13 -0400

    initial commit

---
+++ exercises/quad_sequence.py
@@ -0,0 +1,20 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Quad Sequence:
+Write a function that receives an array of x integers and returns an array of x integers in the Nth term of a quadratic number sequence (where x is the length of the incoming array). Your function should return the continuation of the quadratic sequence of the length equal to the length of the given array.
+
+Examples
+    quadSequence([48, 65, 84]) ➞ [105, 128, 153]
+    quadSequence([0, 1, 6, 15, 28]) ➞ [45, 66, 91, 120, 153]
+    quadSequence([9, 20, 33, 48]) ➞ [65, 84, 105, 128]
+
+Started: April 19, 2024 @ 7:10am ET
+Intervals: 1
+Ended: April 19, 2024 @ 7:40am ET
+"""
