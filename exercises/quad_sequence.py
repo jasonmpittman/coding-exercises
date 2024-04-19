8328b4b19c7246b9e6ed849411184d5d7c03b85d
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-19 07:40:32 -0400

    partial implementation of quadractic function

--- exercises/quad_sequence.py
+++ exercises/quad_sequence.py
@@ -18,3 +18,34 @@ Started: April 19, 2024 @ 7:10am ET
 Intervals: 1
 Ended: April 19, 2024 @ 7:40am ET
 """
+from sys import argv
+
+# TODO: follow algorithm here: https://thirdspacelearning.com/gcse-maths/algebra/quadratic-sequences/
+def generate_sequence(sequence: list) -> list:
+    # an**2 + bn + c
+    first_differences = []
+    second_differences = []
+    halved_difference = 0
+    length = len(sequence)
+
+    # find first difference between terms
+    for i in range(length - 1):
+        first_differences.append(sequence[i + 1] - sequence[i])
+
+    # find second difference between terms
+    for i in range(len(first_differences) - 1):
+        second_differences.append(first_differences[i + 1] - first_differences[i])
+
+    # half the second difference (we assume it is the same value...should check b/c not quadractic if false)
+    halved_difference = second_differences[0] // 2
+    
+    # substract an**2 from the original sequence... so 48 - 48*1**2 = 0, 65 - 65*2**2 = 0, 84 - 84*1**2 = 0
+
+    # is new sequence linear?
+
+
+
+if __name__ == "__main__":
+    sequence = list(map(int, argv[1].split(','))) # 48,65,84
+
+    generate_sequence(sequence)
