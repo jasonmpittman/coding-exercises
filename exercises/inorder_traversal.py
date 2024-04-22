45ae6bb39aebf317851fa703258259f264ba5b50
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-22 05:58:19 -0400

    initial commit

---
+++ exercises/inorder_traversal.py
@@ -0,0 +1,29 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Inorder Traversal:
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Input:
+		   1
+		 /   \
+		/	  \
+	   2	   3
+	  /		  / \
+	 /	  	 /	 \
+	4		5	  6
+		   / \
+		  /   \
+		 7	   8
+
+Output: [4, 2, 1, 7, 5, 8, 3, 6]
+
+Started: April 22, 2024 @ 6:00am ET
+Intervals: 1
+Ended: April 22, 2024 @ 6:30am ET
+"""
