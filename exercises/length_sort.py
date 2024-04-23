4c3d256d42b302b6392b7db88c8d6d9e6facf334
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-23 04:56:59 -0400

    initial commit

---
+++ exercises/length_sort.py
@@ -0,0 +1,21 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Length Sort:
+Create a sorting function which sorts numbers not by numerical order, but by number length! This means sorting numbers with the least amount of digits first, up to the numbers with the most digits.
+
+Examples:
+    numberLenSort([1, 54, 1, 2, 463, 2]) ➞ [1, 1, 2, 2, 54, 463]
+    numberLenSort([999, 421, 22, 990, 32]) ➞ [22, 32, 999, 421, 990]
+    numberLenSort([9, 8, 7, 6, 5, 4, 31, 2, 1, 3]) ➞ [9, 8, 7, 6, 5, 4, 2, 1, 3, 31]
+
+
+Started: April 23, 2024 @ 5:00am ET
+Intervals: 1
+Ended: April 23, 2024 @ 5:30am ET
+"""
