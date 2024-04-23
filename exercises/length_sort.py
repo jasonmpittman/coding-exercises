a1dbdfa648106a622a62b0cf9000800e9aa4a661
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-23 05:22:01 -0400

    implemented sort length function

--- exercises/length_sort.py
+++ exercises/length_sort.py
@@ -17,5 +17,38 @@ Examples:
 
 Started: April 23, 2024 @ 5:00am ET
 Intervals: 1
-Ended: April 23, 2024 @ 5:30am ET
+Ended: April 23, 2024 @ 5:17am ET
 """
+from sys import argv
+
+def sort_by_length(array: list, mode="default") -> list:
+    sorted_array = []
+    i = 0
+
+    # finished at 5:21 after reading about sorted()
+    if mode == "optimized":
+        return sorted(array, key=len)
+
+    # finished this @ 5:17
+    if mode == "default":
+        for a in array:
+            print(a)
+            if len(sorted_array) == 0:
+                sorted_array.append(array[i])
+                i += 1
+            elif len(a) >= len(sorted_array[-1]):
+                print("appending ", a)
+                sorted_array.append(a)
+                i += 1
+            else:
+                print("pre-pending ", a)
+                sorted_array.insert(0, a)
+                i += 1
+
+        return sorted_array
+
+if __name__ == "__main__":
+    array = argv[1].split(',') # 1,54,1,2,463,2     999,421,22,990,32
+
+    sorted_array = sort_by_length(array, mode="optimized")
+    print(sorted_array)
