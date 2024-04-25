4dd45db632818a67f41c6a43bdc187c687bbebf5
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-25 06:25:20 -0400

    initial commit

---
+++ exercises/zero_sum.py
@@ -0,0 +1,30 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Zero Sum: 
+Given an integer array, check if it contains a contiguous subarray having zero-sum.
+
+Input : [3, 4, -7, 3, 1, 3, 1, -4, -2, -2]
+Output: True
+Explanation: The subarrays with zero-sum are
+
+[3, 4, -7]
+[4, -7, 3]
+[-7, 3, 1, 3]
+[3, 1, -4]
+[3, 1, 3, 1, -4, -2, -2]
+[3, 4, -7, 3, 1, 3, 1, -4, -2, -2]
+
+Input : [4, -7, 1, -2, -1]
+Output: False
+
+Started: April 25, 2024 @ 6:25am ET
+Intervals: 1
+Ended: April 25, 2024 @ 6:55am ET
+"""
+
