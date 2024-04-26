09d9d6b2801ec84f92273f8743fa1b62110df22a
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-26 03:17:29 -0400

    initial commit

---
+++ exercises/kth_largest.py
@@ -0,0 +1,23 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Kth Largest: 
+Given an integer array, find k'th largest element in the array where k is a positive integer less than or equal to the length of array.
+
+Input : [7, 4, 6, 3, 9, 1], k = 2
+Output: 7
+Explanation: The 2nd largest array element is 7
+
+Input : [1, 5, 2, 2, 2, 5, 5, 4], k = 4
+Output: 4
+Explanation: The 4th largest array element is 4
+
+Started: April 26, 2024 @ 3:20am ET
+Intervals: 1
+Ended: April 26, 2024 @ 3:50am ET
+"""
