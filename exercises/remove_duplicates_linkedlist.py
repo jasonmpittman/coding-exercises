4282fc792f409b6db685636dd4e96aadf0a1792b
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-28 04:58:07 -0400

    initial commit

---
+++ exercises/remove_duplicates_linkedlist.py
@@ -0,0 +1,22 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Remove Duplicates:
+Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+Examples:
+    Input: head = [1,1,2]
+    Output: [1,2]
+
+    Input: head = [1,1,2,3,3]
+    Output: [1,2,3]
+
+Started: April 28, 2024 @ 5:00am ET
+Intervals: 1
+Ended: April 28, 2024 @ 5:30am ET
+"""
