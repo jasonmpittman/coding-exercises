143b131ed7c19524113a7dbc7df4dc6b77f8be5c
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-30 02:18:56 -0400

    initial commit

---
+++ exercises/spiral_matrix.py
@@ -0,0 +1,22 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Spiral Matrix:
+Given an m x n matrix, return all elements of the matrix in spiral order.
+
+Examples:
+    Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    Output: [1,2,3,6,9,8,7,4,5]
+
+    Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+    Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+
+Started: April 30, 2024 @ 2:20am ET
+Intervals: 1
+Ended: April 30, 2024 @ 2:50am ET
+"""
