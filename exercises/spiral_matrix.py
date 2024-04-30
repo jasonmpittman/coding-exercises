4bff11b8d0c4f0033417f71e62fc6f013a441478
Jason M. Pittman <jason@jasonmpittman.com>
2024-04-30 02:47:57 -0400

    implemented find spiral function

--- exercises/spiral_matrix.py
+++ exercises/spiral_matrix.py
@@ -18,5 +18,46 @@ Examples:
 
 Started: April 30, 2024 @ 2:20am ET
 Intervals: 1
-Ended: April 30, 2024 @ 2:50am ET
+Ended: April 30, 2024 @ 2:47am ET
 """
+
+# assumes 3xn matrix
+def find_spiral(matrix: list) -> list:
+    m = len(matrix) #rows
+    n = len(matrix[0]) #columns
+    spiral = []
+
+    # iterate first row
+    for i in matrix[0]:
+        spiral.append(i)
+
+    # iterate last column
+    last_column = [col[n - 1] for col in matrix]
+    for i in last_column[1:]:
+        spiral.append(i)
+
+    # iterate last row in reverse
+    for i in sorted(matrix[m - 1], reverse=True):
+        if i not in spiral:
+            spiral.append(i)
+
+    # iterate first column in reverse minus first row
+    first_column = [col[0] for col in matrix]
+    print(first_column)
+    for i in sorted(first_column, reverse=True):
+        if i not in spiral:
+            spiral.append(i)
+
+    # iterature middle row minus last column
+    for i in matrix[1]:
+        if i not in spiral:
+            spiral.append(i)
+    
+    return spiral
+
+if __name__ == "__main__":
+    matrix = [[1,2,3], [4,5,6], [7,8,9]]
+    #matrix = [[1,2,3,4], [5,6,7,8], [9,10,11,12]]
+
+    spiral = find_spiral(matrix)
+    print(spiral)
