a0c00644b3dd1cc96cd27b864883c12c0e2a9e04
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-01 03:16:22 -0400

    initial commit

---
+++ exercises/climbing_stairs.py
@@ -0,0 +1,34 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Climbing Stairs:
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+
+Started: May 01, 2024 @ 3:20am ET
+Intervals: 1
+Ended: May 01, 2024 @ 3:50am ET
+"""
