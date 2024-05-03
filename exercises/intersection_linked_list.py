d1296e3d7007c2e8dd3b93976b81c94d83a64e68
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-03 04:04:33 -0400

    implemented partial function- want diff problem

--- exercises/intersection_linked_list.py
+++ exercises/intersection_linked_list.py
@@ -9,7 +9,12 @@ __status__ = "Exercise"
 """ Intersection Linked List:
 Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
 
+Examples:
+    Input: intersectVal = 8, listA = [4,1,8,4,5], listB = [5,6,1,8,4,5], skipA = 2, skipB = 3
+    Output: Intersected at '8'
 
+    Input: intersectVal = 2, listA = [1,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+    Output: Intersected at '2'
 
 Started: May 03, 2024 @ 3:45am ET
 Intervals: 1
@@ -22,4 +27,29 @@ class ListNode:
         self.next = None
 
 
+def find_intersection(headA: ListNode, headB: ListNode) -> ListNode:
+    a, b = headA, headB
 
+    while (a != b):
+        a = headB if not a else a.next
+        b = headA if not b else b.next
+    
+    return a
+
+if __name__ == "__main__":
+    a = [4,1,8,4,5]
+    b = [5,6,1,8,4,5]
+    headA = ListNode(a[0])
+    listA = headA
+    headB = ListNode(b[0])
+    listB = headB
+    
+    for _ in a[1:]:
+        listA.next = ListNode(_)
+        listA = listA.next
+
+    for _ in b[1:]:
+        listB.next = ListNode(_)
+        listB = listB.next
+
+    
