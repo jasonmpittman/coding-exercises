11fc8038f1634d10a2fd1cb73d76bc453c586233
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-03 03:42:28 -0400

    initial commit

---
+++ exercises/intersection_linked_list.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Intersection Linked List:
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+
+
+Started: May 03, 2024 @ 3:45am ET
+Intervals: 1
+Ended: May 03, 2024 @ 4:15am ET
+"""
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+
