4fee9d26e68419ec7bcb1de265a7779f962ee629
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-04 06:12:00 -0400

    initial commit

---
+++ exercises/list_filtering.py
@@ -0,0 +1,20 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" List Filtering:
+Create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Example
+    filter_list([1,2,'a','b']) == [1,2]
+    filter_list([1,'a','b',0,15]) == [1,0,15]
+    filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+Started: May 04, 2024 @ 6:15am ET
+Intervals: 1
+Ended: May 04, 2024 @ 6:45am ET
+"""
