fb8907c40936145cd3448270260b4a903815fd53
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-04 06:32:50 -0400

    implemented two filtering functions

--- exercises/list_filtering.py
+++ exercises/list_filtering.py
@@ -18,3 +18,36 @@ Started: May 04, 2024 @ 6:15am ET
 Intervals: 1
 Ended: May 04, 2024 @ 6:45am ET
 """
+from sys import argv
+
+def filter_numbers(element: str) -> bool:
+    numbers = [str(x) for x in range(0,1000)]
+
+    if element in numbers:
+        return True
+    else:
+        return False
+
+def filter_letters(element: str) -> bool:
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    letters = [a for a in alphabet] #list(alphabet)
+    
+    if element in letters:
+        return True
+    else:
+        return False 
+
+def filter_list(the_list: list, filter_type='default') -> list:
+
+    if filter_type == 'default':
+        return list(filter(filter_letters, the_list))
+    
+    if filter_type == 'numeric':
+        return list(filter(filter_numbers, the_list))
+
+if __name__ == "__main__":
+    the_list = argv[1].split(',')
+
+    filtered_list = filter_list(the_list, filter_type='numeric')
+    print(filtered_list)
+    
