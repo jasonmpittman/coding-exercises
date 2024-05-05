363d8c92603e2daac688fc9fd54fd5392ffa5a26
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-05 05:27:42 -0400

    initial commit

---
+++ exercises/sum_of_dice.py
@@ -0,0 +1,26 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Sum of Dice:
+You have n dices each one having s sides numbered from 1 to s. How many outcomes add up to a specified number k?
+
+For example if we roll four normal six-sided dices we have four outcomes that add up to 5.
+
+(1, 1, 1, 2) (1, 1, 2, 1) (1, 2, 1, 1) (2, 1, 1, 1)
+
+There are 100 random tests with:
+
+    0 <= n <= 10
+    0 <= s <= 20
+    0 <= k <= n * s
+
+
+Started: May 05, 2024 @ 5:30am ET
+Intervals: 1
+Ended: May 05, 2024 @ 6:00am ET
+"""
