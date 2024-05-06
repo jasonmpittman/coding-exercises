2fe69f665c00269daaac61a8912b7ad9d5451f3a
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-06 06:02:59 -0400

    initial commit

---
+++ exercises/farm_problem.py
@@ -0,0 +1,26 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Farm Problem:
+In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+
+    chickens = 2 legs
+    cows = 4 legs
+    pigs = 4 legs
+
+The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
+
+Examples
+    animals(2, 3, 5) ➞ 36
+    animals(1, 2, 3) ➞ 22
+    animals(5, 2, 8) ➞ 50
+
+Started: May 06, 2024 @ 6:05am ET
+Intervals: 1
+Ended: May 06, 2024 @ 6:35am ET
+"""
