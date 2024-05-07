19bbc9417fbd6c3652b2393d1ab59878e1a9fa9f
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-07 05:48:10 -0400

    initial commit

---
+++ exercises/least_larger.py
@@ -0,0 +1,19 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Least Larger:
+Given an array of numbers and an index, return either the index of the smallest number that is larger than the element at the given index, or -1 if there is no such index ( or, where applicable, Nothing or a similarly empty value ).
+
+Examples:
+    leastLarger( [4, 1, 3, 5, 6], 0 )  =>  3
+    leastLarger( [4, 1, 3, 5, 6], 4 )  => -1
+
+Started: May 07, 2024 @ 5:50am ET
+Intervals: 1
+Ended: May 07, 2024 @ 6:20am ET
+"""
