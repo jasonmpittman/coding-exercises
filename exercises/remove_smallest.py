24ed178d131d66f754ccf85392734a344116352b
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-08 05:53:53 -0400

    initial commit

---
+++ exercises/remove_smallest.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Remove Smallest:
+Create a function that takes an array of integers and removes the smallest value.
+
+Examples
+    removeSmallest([1, 2, 3, 4, 5] ) ➞ [2, 3, 4, 5]
+    removeSmallest([5, 3, 2, 1, 4]) ➞ [5, 3, 2, 4]
+    removeSmallest([2, 2, 1, 2, 1]) ➞ [2, 2, 2, 1]
+
+Notes
+    Don't change the order of the left over items.
+    If you get an empty array, return an empty array: [] ➞ []
+    If there are multiple items with the same value, remove item with lower index (3rd example).
+
+Started: May 08, 2024 @ 5:55am ET
+Intervals: 1
+Ended: May 08, 2024 @ 6:25am ET    
+"""
