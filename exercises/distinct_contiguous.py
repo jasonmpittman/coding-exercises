3282f5914d4caded8ea958bc110bed11e3a117d1
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-09 09:46:21 -0400

    added count distinct functions

--- exercises/distinct_contiguous.py
+++ exercises/distinct_contiguous.py
@@ -22,5 +22,44 @@ Example
 
 Started: May 08, 2024 @ 8:05am ET
 Intervals: 1
-Ended: May 08, 2024 @ 8:35am ET
+Ended: May 08, 2024 @ 9:45am ET #pause 820 unpause 940
 """
+from sys import argv
+
+def get_windows(array: list, k: int) -> list:
+    windows = []
+    start = 0
+    end = k
+
+    while end <= (len(array)):
+        temp = []
+        for n in range(start, end):
+            temp.append(array[n])
+
+        start += 1
+        end += 1
+        windows.append(temp)
+
+    return windows 
+
+
+def count_distinct(array: list, k: int) -> list:
+    counts = []
+    
+    # get possible windows
+    windows = get_windows(array, k)
+    print(windows)
+
+    # count distinct number in each window
+    for window in windows:
+        counts.append(len(set(window)))
+    
+    return counts
+
+# note: the problem asks just for distinct, not truly contiguous (i.e., 1,2,3) distinct
+if __name__ == "__main__":
+    array = list(map(int, argv[1].split(','))) # 1,2,1,3,4,2,3 4
+    k = int(argv[2])
+
+    counts = count_distinct(array, k)
+    print(counts)
