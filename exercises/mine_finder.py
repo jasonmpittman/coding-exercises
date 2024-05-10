2a2ed9202e58324fc779dfbff14f268c1ec0b7de
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-10 04:07:24 -0400

    initial commit

---
+++ exercises/mine_finder.py
@@ -0,0 +1,20 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Mine Finder:
+Write a function mineLocation/MineLocation that accepts a 2D array, and returns the location of the mine. The mine is represented as the integer 1 in the 2D array. Areas in the 2D array that are not the mine will be represented as 0s. 
+
+Examples:
+    mineLocation( [ [1, 0, 0], [0, 0, 0], [0, 0, 0] ] ) => returns [0, 0]
+    mineLocation( [ [0, 0, 0], [0, 1, 0], [0, 0, 0] ] ) => returns [1, 1]
+    mineLocation( [ [0, 0, 0], [0, 0, 0], [0, 1, 0] ] ) => returns [2, 1]
+
+Started: May 10, 2024 @ 4:10am ET
+Intervals: 1
+Ended: May 10, 2024 @ 4:40am 
+"""
