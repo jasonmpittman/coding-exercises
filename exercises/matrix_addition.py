d3e47f208d3904bd4cc80ca871da43a1fd7950bb
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-10 04:05:35 -0400

    implemented addition function for matrices

--- exercises/matrix_addition.py
+++ exercises/matrix_addition.py
@@ -26,5 +26,30 @@ Examples:
 
 Started: May 10, 2024 @ 4:00am ET
 Intervals: 1
-Ended: May 10, 2024 @ 4:30am        
+Ended: May 10, 2024 @ 4:05am        
 """
+
+def add_matrices(a: list, b: list) -> list:
+    c = []
+
+    for i in range(len(a)):
+        row = []
+        for j in range(len(a[i])):
+            row.append(a[i][j] + b[i][j]) 
+        
+        c.append(row)
+
+    return c
+
+if __name__ == "__main__":
+    a = [ [1, 2, 3],
+        [3, 2, 1],
+        [1, 1, 1] ]
+    
+    b = [ [2, 2, 1],
+        [3, 2, 3],
+        [1, 1, 3] ]
+
+    c = add_matrices(a, b)
+    print(c)
+    
