94ac767ce7490bdd0dbcafeba5adcd79eb90f21d
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-10 04:19:20 -0400

    did i forget to commit this?

--- exercises/least_larger.py
+++ exercises/least_larger.py
@@ -15,5 +15,31 @@ Examples:
 
 Started: May 07, 2024 @ 5:50am ET
 Intervals: 1
-Ended: May 07, 2024 @ 6:20am ET
+Ended: May 07, 2024 @ 5:59am ET
 """
+from sys import argv
+
+def find_least_larger(array: list, index: int) -> int:
+    target = array[index]
+
+    # sort array
+    sorted_array = sorted(array)
+    
+    # find target index
+    target_index = sorted_array.index(target)
+
+    # return new index + 1 if new index is not last in array
+    if target_index is not (len(sorted_array) - 1):
+        return target_index + 1 
+    else:
+        return '-1'
+
+if __name__ == "__main__":
+    array = list(map(int, argv[1].split(',')))
+    index = int(argv[2])
+
+    if len(array) == 0:
+        print('-1')
+    else:
+        result = find_least_larger(array, index) # 4,1,3,5,6 0  4,1,3,5,6 4
+        print(result)
