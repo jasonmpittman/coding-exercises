d282c23a5f7b226bdc3cb9c84a0cfc8a850edf1c
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-11 05:39:54 -0400

    initial commit

---
+++ exercises/collection_differences.py
@@ -0,0 +1,22 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Collection Differences:
+Find the difference between two collections. The difference means that either the character is present in one collection or it is present in other, but not in both. Return a sorted list with the difference.
+
+The collections can contain any character and can contain duplicates.
+
+Example
+    A = [a, a, t, e, f, i, j]
+    B = [t, g, g, i, k, f]
+    difference = [a, e, g, j, k]
+
+Started: May 11, 2024 @ 5:40am ET
+Intervals: 1
+Ended: May 11, 2024 @ 6:10am 
+"""
