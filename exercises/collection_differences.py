654e96b098e26144961dbcac2e21956757104a3a
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-11 05:50:39 -0400

    implemented differences function with modes

--- exercises/collection_differences.py
+++ exercises/collection_differences.py
@@ -20,3 +20,30 @@ Started: May 11, 2024 @ 5:40am ET
 Intervals: 1
 Ended: May 11, 2024 @ 6:10am 
 """
+from sys import argv
+
+def find_differences(A: list, B: list, mode="set") -> list:
+    differences = []
+
+    if mode == "set": #547
+        differences.append(set(A).difference(set(B)))
+        differences.append(set(B).difference(set(A)))
+
+    if mode == "naive": #549
+        for a in A:
+            if a not in B and a not in differences:
+                differences.append(a)
+        
+        for b in B: 
+            if b not in A and b not in differences:
+                differences.append(b)
+
+    return differences
+
+
+if __name__ == "__main__":
+    A = argv[1].split(',') # a,a,t,e,f,i,j t,g,g,i,k,f
+    B = argv[2].split(',')
+
+    differences = find_differences(A, B, "naive")
+    print(differences)
