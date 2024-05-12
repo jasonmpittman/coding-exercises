02aeb8b1e0f10de8f5ff3fbb755f52c6171f205d
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-12 06:59:34 -0400

    initial commit

---
+++ exercises/difference_of_two.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Difference of Two:
+The objective is to return all pairs of integers from a given array of integers that have a difference of 2.
+
+The result array should be sorted in ascending order of values.
+
+Assume there are no duplicate integers in the array. The order of the integers in the input array should not matter.
+
+Examples
+    [1, 2, 3, 4]  should return [[1, 3], [2, 4]]
+    [4, 1, 2, 3]  should also return [[1, 3], [2, 4]]
+    [1, 23, 3, 4, 7] should return [[1, 3]]
+    [4, 3, 1, 5, 6] should return [[1, 3], [3, 5], [4, 6]]
+
+Started: May 12, 2024 @ 7:00am ET
+Intervals: 1
+Ended: May 12, 2024 @ 7:30am
+"""
