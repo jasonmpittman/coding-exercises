867536cd8916abea73cc5a46eefe3a0d3fb1fd60
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-13 07:45:41 -0400

    implemented score function

--- exercises/greed_game.py
+++ exercises/greed_game.py
@@ -31,5 +31,59 @@ Example scoring
 
 Started: May 13, 2024 @ 7:30am ET
 Intervals: 1
-Ended: May 13, 2024 @ 8:00am
+Ended: May 13, 2024 @ 7:45am
 """
+from sys import argv
+
+def score_dice(dice: list) -> int:
+    score = 0
+
+    if dice.count('1') >= 3:
+        score += 1000
+
+        for _ in range(3):
+            dice.remove('1')
+    
+    if dice.count('6') >= 3:
+        score += 600
+        
+        for _ in range(3):
+            dice.remove('6')
+
+    if dice.count('5') >= 3:
+        score += 500
+                
+        for _ in range(3):
+            dice.remove('5')
+    
+    if dice.count('4') >= 3:
+        score += 400
+
+        for _ in range(3):
+            dice.remove('4')
+
+    if dice.count('3') >= 3:
+        score += 300
+
+        for _ in range(3):
+            dice.remove('3')
+    
+    if dice.count('2') >= 3:
+        score += 200
+
+        for _ in range(3):
+            dice.remove('2')
+    
+    if dice.count('1') <= 2:
+        score += 100 * dice.count('1')
+    
+    if dice.count('5') <= 2:
+        score += 50 * dice.count('5')
+
+    return score
+
+if __name__ == "__main__":
+    dice = argv[1].split(',')
+
+    score = score_dice(dice) # 5,1,3,4,1
+    print(score)
