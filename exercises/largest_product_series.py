1e81ba479ce32cd8f8ad7ad282b3108650bef4a9
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-14 03:57:40 -0400

    implemented product function

--- exercises/largest_product_series.py
+++ exercises/largest_product_series.py
@@ -14,5 +14,33 @@ Example:
 
 Started: May 14, 2024 @ 3:45am ET
 Intervals: 1
-Ended: May 14, 2024 @ 4:15am
+Ended: May 14, 2024 @ 3:55am
 """
+from sys import argv
+import functools
+import operator
+
+def find_greatest_product(series: str) -> int:
+    head = 0
+    tail = 5
+    greatest = 0
+
+    while tail <= len(series):
+        subseries = series[head:tail]
+        product = functools.reduce(operator.mul, [int(x) for x in subseries])
+    
+        if product > greatest:
+            greatest = product
+        
+        head += 1
+        tail += 1
+
+        #print("Current greatest is: ", greatest)
+
+    return greatest
+
+if __name__ == "__main__":
+    series = argv[1] # 123834539327238239583 # 92494737828244222221111111532909999 -> 5292
+
+    product = find_greatest_product(series)
+    print(product)
