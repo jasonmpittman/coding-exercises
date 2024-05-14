f5ba4a3226293b24f66a2da79e710c2688406834
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-14 03:43:10 -0400

    initial commit

---
+++ exercises/largest_product_series.py
@@ -0,0 +1,18 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Largest Product Series:
+Write a function that will find the greatest product of five consecutive digits in the given string of digits.
+
+Example: 
+    "123834539327238239583" is 3240.
+
+Started: May 14, 2024 @ 3:45am ET
+Intervals: 1
+Ended: May 14, 2024 @ 4:15am
+"""
