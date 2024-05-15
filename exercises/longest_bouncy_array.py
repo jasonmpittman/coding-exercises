ccececb3cf21a157255cc55156dd0a10d5f4a8de
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-15 04:33:30 -0400

    initial commit

---
+++ exercises/longest_bouncy_array.py
@@ -0,0 +1,21 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Longest Bouncy Subarray:
+Write a function to output the longest strict bouncy subarray from a given array.
+
+In the case of having more than one bouncy subarray of same length, it should be output the subarray with its first term of lowest index in the original array.
+
+Examples:
+    arr = [7,9,6,10,5,11,10,12,13,4,2,5,1,6,4,8]
+    longest_bouncy_list(arr) === [7,9,6,10,5,11,10,12] 
+
+Started: May 15, 2024 @ 4:35am ET
+Intervals: 1
+Ended: May 15, 2024 @ 5:05am
+"""
