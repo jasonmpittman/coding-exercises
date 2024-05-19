3b572a1d7dd022ff23fd7fc11eb58a359a75b25d
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-19 04:40:06 -0400

    initial commit

---
+++ exercises/missing_element.py
@@ -0,0 +1,22 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Missing Element:
+Given two integer arrays where the second array is a shuffled duplicate of the first array with one element missing, find the missing element.
+
+Please note, there may be duplicates in the arrays, so checking if a numerical value exists in one and not the other is not a valid solution.
+
+Examples:
+    find_missing([1, 2, 2, 3], [1, 2, 3]) => 2
+    find_missing([6, 1, 3, 6, 8, 2], [3, 6, 6, 1, 2]) => 8
+
+
+Started: May 19, 2024 @ 4:40am ET
+Intervals: 1
+Ended: May 19, 2024 @ 5:10am
+"""
