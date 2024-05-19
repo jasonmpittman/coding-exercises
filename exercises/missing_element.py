fa6eef84b35989c2e293f985f789c2fe6388f2cd
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-19 04:54:55 -0400

    implemented missing element function

--- exercises/missing_element.py
+++ exercises/missing_element.py
@@ -18,5 +18,22 @@ Examples:
 
 Started: May 19, 2024 @ 4:40am ET
 Intervals: 1
-Ended: May 19, 2024 @ 5:10am
+Ended: May 19, 2024 @ 4:54am
 """
+from sys import argv
+
+def find_missing_element(A: list, B: list) -> int:
+
+    # mutate the larger list so what is left is the missing element
+    for i in range(len(B)):
+        if B[i] in A:
+            A.remove(B[i])
+
+    return A
+
+if __name__ == "__main__":
+    A = list(map(int, argv[1].split(','))) # 1,2,2,3 1,2,3 # 6,1,3,6,8,2 3,6,6,1,2
+    B = list(map(int, argv[2].split(',')))
+
+    missing_element = find_missing_element(A, B)
+    print(missing_element)
