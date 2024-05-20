a8864ac0fbd1dd341c94377f7dcf4c6a69214ed8
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-20 06:24:56 -0400

    initial commit

---
+++ exercises/split_pad.py
@@ -0,0 +1,19 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Split Pad:
+Complete the function that receives 3 arguments: n, array, padding and returns a new array that contains the result of splitting array into arrays of size n. If the last array doesn't have enough items, pad it with padding so that it becomes equal in size to the other split arrays.
+
+Examples
+    2, [1, 2, 3, 4], nil  ==>  [ [1, 2], [3, 4] ]
+    2, [1, 2, 3], nil     ==>  [ [1, 2], [3, nil] ]
+
+Started: May 20, 2024 @ 6:25am ET
+Intervals: 1
+Ended: May 20, 2024 @ 6:55am
+"""
