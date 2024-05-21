678814e61a35be9751fcbb2b0d1e405ee83e0a21
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-21 16:02:25 -0400

    initial commit planning ahead

---
+++ exercises/max_divisors.py
@@ -0,0 +1,22 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Max Divisors:
+Find the integer from a to b (included) with the greatest number of divisors. For example:
+
+divNum(15, 30)   ==> 24
+divNum(1, 2)     ==> 2
+divNum(0, 0)     ==> 0
+divNum(52, 156)  ==> 120
+
+If there are several numbers that have the same (maximum) number of divisors, the smallest among them should be returned. Return the string "Error" if a > b.
+
+Started: May 27, 2024 @ 6:00am ET
+Intervals: 1
+Ended: May 27, 2024 @ 6:30am ET
+"""
---
+++ exercises/remove_marked.py
@@ -0,0 +1,20 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Remove Marked:
+Create a function that removes from a given array of integers all the values contained in a second array.
+
+Examples (input -> output):
+    * [1, 1, 2, 3, 1, 2, 3, 4], [1, 3] -> [2, 2, 4]
+    * [1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2] -> [5, 6, 7, 8]
+    * [8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3] -> [8, 7, 6, 5, 1]
+
+Started: May 25, 2024 @ 6:00am ET
+Intervals: 1
+Ended: May 25, 2024 @ 6:30am ET
+"""
