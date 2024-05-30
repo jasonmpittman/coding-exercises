66dd0aab895e08958d839d633211fcc9ca9c981b
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-30 05:38:38 -0400

    initial commit

---
+++ exercises/rotate_matrix.py
@@ -0,0 +1,23 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Rotate Matrix:
+Rotate a given matrix 90 degrees to the left.
+
+Example:
+    Input: {{-1, 4, 5},
+            { 2, 3, 4}}
+
+    Output: {{ 5, 4},
+            { 4, 3},
+            {-1, 2}}
+
+Started: May 30, 2024 @ 5:40am ET
+Intervals: 1
+Ended: May 30, 2024 @ 6:10am ET
+"""
