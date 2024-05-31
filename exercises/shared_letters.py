e43047e78d20e1b47b74f38b2916ed861b8ed57d
Jason M. Pittman <jason@jasonmpittman.com>
2024-05-31 07:16:37 -0400

    initial commit

---
+++ exercises/shared_letters.py
@@ -0,0 +1,23 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Shared Letters:
+Create a function that returns the number of characters shared between two words.
+
+Examples
+    sharedLetters("apple", "meaty") ➞ 2
+    // Since "ea" is shared between "apple" and "meaty".
+
+    sharedLetters("fan", "forsook") ➞ 1
+
+    sharedLetters("spout", "shout") ➞ 4
+
+Started: May 31, 2024 @ 7:15am ET
+Intervals: 1
+Ended: May 31, 2024 @ 7:45am ET
+"""
