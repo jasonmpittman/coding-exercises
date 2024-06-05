5cdee619530e5487bc13587b893f3aa761214748
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-05 06:22:19 -0400

    initial commit

---
+++ exercises/job_sequencing.py
@@ -0,0 +1,14 @@
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Job Sequencing:
+Given a list of tasks with deadlines and total profit earned on completing a task, find the maximum profit earned by executing the tasks within the specified deadlines. Assume that each task takes one unit of time to complete, and a task can’t execute beyond its deadline. Also, only a single task will be executed at a time.
+
+Started: June 05, 2024 @ 6:25am ET
+Intervals: 1
+Ended: June 05, 2024 @ 6:55am ET
+"""
