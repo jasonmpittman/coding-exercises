33869d3e4d350496e67833fad3cab708c351a972
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-07 06:27:20 -0400

    initial commit

---
+++ exercises/sum_numbers.py
@@ -0,0 +1,22 @@
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Sum Numbers:
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Examples (a, b) --> output (explanation)
+    (1, 0) --> 1 (1 + 0 = 1)
+    (1, 2) --> 3 (1 + 2 = 3)
+    (0, 1) --> 1 (0 + 1 = 1)
+    (1, 1) --> 1 (1 since both are same)
+    (-1, 0) --> -1 (-1 + 0 = -1)
+    (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+Started: June 07, 2024 @ 6:30am ET
+Intervals: 1
+Ended: June 07, 2024 @ 7:00am ET
+"""
