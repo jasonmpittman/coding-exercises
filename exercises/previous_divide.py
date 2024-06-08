cfe008d983e5bd52f6d910cfab30a107aaa4c2d8
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-08 05:19:29 -0400

    initial commit

---
+++ exercises/previous_divide.py
@@ -0,0 +1,23 @@
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Previous Divide:
+Take a number and check each digit if it is divisible by the digit on its left checked and return an array of booleans.
+
+The booleans should always start with false becase there is no digit before the first one.
+
+Examples
+    73312        => [false, false, true, false, true]
+    2026         => [false, true, false, true]
+    635          => [false, false, false]
+
+*** Remember 0 is evenly divisible by all integers but not the other way around ***
+
+Started: June 08, 2024 @ 5:20am ET
+Intervals: 1
+Ended: June 08, 2024 @ 5:50am ET
+"""
