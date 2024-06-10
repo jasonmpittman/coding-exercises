9f6f9c7218b2ccf8d8f1c5e455ece01c64b1256f
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-10 04:41:41 -0400

    initial commit

---
+++ exercises/move_zeroes.py
@@ -0,0 +1,17 @@
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Move Zeroes:
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+Example:
+    moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+Started: June 10, 2024 @ 4:40am ET
+Intervals: 1
+Ended: June 10, 2024 @ 5:10am ET
+"""
