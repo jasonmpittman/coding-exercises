dea876abe06a36407d964ba4308d1e4c995b09d5
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-11 05:32:32 -0400

    initial commit

---
+++ exercises/pangrams.py
@@ -0,0 +1,17 @@
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Pangrams:
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+
+Started: June 11, 2024 @ 5:35am ET
+Intervals: 1
+Ended: June 11, 2024 @ 6:05am ET
+"""
