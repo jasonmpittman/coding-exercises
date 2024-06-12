08c8516b2bb23982289502239f17e5b2c8520813
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-12 05:57:29 -0400

    added recursive deep count function

--- exercises/deep_count.py
+++ exercises/deep_count.py
@@ -16,5 +16,23 @@ Examples
 
 Started: June 12, 2024 @ 5:35am ET
 Intervals: 1
-Ended: June 12, 2024 @ 6:05am ET
+Ended: June 12, 2024 @ 5:56am ET
 """
+
+
+def count_deep(array: list) -> int:
+    """Returns count of all elements within an array or nested arrays"""
+    count = 0
+
+    for a in array:
+        count += 1
+        if isinstance(a, list):
+            count += count_deep(a)
+
+    return count
+
+if __name__ == "__main__":
+    array = ['x', 'y', ['z']] # [1, 2, [3, 4, [5]]] # [1, 2, 3] # ['x', 'y', ['z']]
+
+    count = count_deep(array)
+    print(count)
