367558c5dfb4b415d8b90c7bfbe1a622932ee0a6
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-12 05:36:16 -0400

    initial commit

---
+++ exercises/deep_count.py
@@ -0,0 +1,20 @@
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Deep Count:
+You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+
+Examples
+    []                   -->  0
+    [1, 2, 3]            -->  3
+    ["x", "y", ["z"]]    -->  4
+    [1, 2, [3, 4, [5]]]  -->  7
+
+Started: June 12, 2024 @ 5:35am ET
+Intervals: 1
+Ended: June 12, 2024 @ 6:05am ET
+"""
