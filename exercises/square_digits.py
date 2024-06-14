26ea589e49b3b12447e3c8c6801e03baeb46bbd2
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-14 10:31:03 -0400

    initial commit

---
+++ exercises/square_digits.py
@@ -0,0 +1,18 @@
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Square Digits:
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+Started: June 14, 2024 @ 10:30am ET
+Intervals: 1
+Ended: June 14, 2024 @ 11:00am ET
+"""
