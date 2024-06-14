a2bc8ee6c75be2f8c915467cd43f931943b71bc6
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-14 10:21:30 -0400

    initial commit

---
+++ exercises/merge_sort.py
@@ -0,0 +1,18 @@
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+
+""" Merge Sort:
+
+    Divide the unsorted array into n subarrays, each of size 1 (an array of size 1 is considered sorted).
+    Repeatedly merge subarrays to produce new sorted subarrays until only 1 subarray is left, which would be our sorted array.
+
+
+Started: June 14, 2024 @ 10:20am ET
+Intervals: 1
+Ended: June 14, 2024 @ 10:50am ET
+"""
