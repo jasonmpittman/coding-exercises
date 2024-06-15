dd0e0d60eb33db32e02bcd8a71ed21cafc1cc29a
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-15 07:00:27 -0400

    initial commit

---
+++ exercises/target_sum.py
@@ -0,0 +1,18 @@
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Target Sum:
+Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in a tuple / list (depending on your language) like so: (index1, index2).
+
+Examples:
+    twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0]
+    twoSum([3, 2, 4], 6) // returns [1, 2] or [2, 1]
+
+Started: June 15, 2024 @ 7:00am ET
+Intervals: 1
+Ended: June 15, 2024 @ 7:30am ET
+"""
