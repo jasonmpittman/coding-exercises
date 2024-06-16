4976ed70fb16d41534c77065f0bb8e5c3b49bf26
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-16 09:09:32 -0400

    initial commit

---
+++ exercises/divisible_x.py
@@ -0,0 +1,22 @@
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Divisible X:
+give two parameters m and x, where m defines the max range starting from 1. You have to return the number of the numbers that can be multiple/divisible by x, where x is a string that consist 2 numbers and "and" or "or" example- "2 or 9"
+
+Examples:
+    from 1 to 1000, random chosen number of numbers being multiple of 2 or 9 is 556
+
+    how_many(1000, "2 or 9") # should return 556
+    how_many(1000, "2 and 9") # should return 55
+    how_many(200, "10 or 10") # should return 20
+
+
+Started: June 16, 2024 @ 9:10am ET
+Intervals: 1
+Ended: June 16, 2024 @ 9:40am ET
+"""
