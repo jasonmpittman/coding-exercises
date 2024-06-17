a7154ebf8311e3f5fe3a5d2bb13681568314895e
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-17 05:00:21 -0400

    implemented build triangle function

--- exercises/number_triangle.py
+++ exercises/number_triangle.py
@@ -21,11 +21,27 @@ You will be given an integer n and your task will be to return the sum of the la
 
 Started: June 17, 2024 @ 4:40am ET
 Intervals: 1
-Ended: June 17, 2024 @ 5:20am ET
+Ended: June 17, 2024 @ 5:00am ET
 """
 from sys import argv
 
+def build_triangle(n: int) -> list:
+    triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if j == 0:
+                # First element in the row, has no left element
+                row.append(1)
+            else:
+                # Sum of the number above (triangle[i-1][j]) and the number to the left (row[j-1])
+                above = triangle[i-1][j] if j < len(triangle[i-1]) else 0
+                left = row[j-1]
+                row.append(above + left)
+        triangle.append(row)
 
+    print(sum(triangle[-1]))        
 
 if __name__ == "__main__":
     n = int(argv[1])
+    build_triangle(n)
