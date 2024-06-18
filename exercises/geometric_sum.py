01142b74dabc480170656cd10f5db59f18fe8e53
Jason M. Pittman <jason@jasonmpittman.com>
2024-06-18 03:37:31 -0400

    initial commit

---
+++ exercises/geometric_sum.py
@@ -0,0 +1,24 @@
+__copyright__ = "Copyright 2024"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Geometric Sum:
+
+geometric_sequence_sum(a, r, n), which will help you compute a geometric progression/series.
+
+The parameters provided are as follows:
+
+    a is the first term
+    r is the common ratio
+    n is the amount of terms
+
+Example:
+    geometric_sequence_sum(2, 3, 5) should return 242.
+
+Started: June 18, 2024 @ 3:40am ET
+Intervals: 1
+Ended: June 18, 2024 @ 4:10am ET
+"""
