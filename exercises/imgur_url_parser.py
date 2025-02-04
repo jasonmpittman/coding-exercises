b0a8b1a52cf9d0a9efa6e29cf9011f854f7e9c2a
jasonmpittman <jason@jasonmpittman.com>
2025-02-05 03:50:26 +1030

    implemented all but image parsing

--- exercises/imgur_url_parser.py
+++ exercises/imgur_url_parser.py
@@ -25,5 +25,37 @@ Examples
 
 Started: Feb 05, 2025 @ 3:18am ET
 Intervals: 1
-Ended: Feb 05, 2025 @ 6:00am ET
+Ended: Feb 05, 2025 @ 3:48am ET
 """
+
+from urllib.parse import urlparse
+
+def parse_url(url):
+    link_id = ''
+    link_type = ''
+
+    parsed_url = urlparse(url)
+
+    if '.png' in parsed_url.path:
+        link_type = 'image'
+        link_id = parsed_url.path.split('.')
+    else:
+        junk, link_type, link_id = parsed_url.path.split('/')
+
+    return {'id': link_id, 'type': link_type}
+
+if __name__ == '__main__':
+    #url = "http://imgur.com/a/cjh4E"
+    #url = "http://imgur.com/gallery/59npG"
+    url ="http://i.imgur.com/altd8Ld.png"
+
+    data = parse_url(url)
+    
+    if data['type'] == 'a':
+        print(data['id'], 'album')
+
+    elif data['type'] == 'gallery':
+        print(data['id'], 'gallery')
+
+    else:
+        print(data['id'], 'image')
