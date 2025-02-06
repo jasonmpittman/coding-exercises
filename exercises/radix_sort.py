f12c76fecef542975e0d1efaf74fa3a1cfd4f5d2
jasonmpittman <jason@jasonmpittman.com>
2025-02-07 04:38:31 +1030

    initial commit

---
+++ exercises/radix_sort.py
@@ -0,0 +1,26 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Radix Sort:
+
+Python has a beautiful built-in function sorted that sorts an iterable, usually an array of numbers, sorting them in ascending order, but using key= you can sort the iterable in different ways.
+
+Create a function that takes an array of integers as an argument and returns the same array in ascending order. Using sorted() would be easy, but for this challenge YOU have to sort the array creating your own algorithm.
+
+    Examples
+
+    sort_array([2, -5, 1, 4, 7, 8]) ➞ [-5, 1, 2, 4, 7, 8]
+
+    sort_array([23, 15, 34, 17, -28]) ➞ [-28, 15, 17, 23, 34]
+
+    sort_array([38, 57, 45, 18, 47, 39]) ➞ [18, 38, 39, 45, 47, 57]
+
+Started: Feb 07, 2025 @ 4:40am ET
+Intervals: 1
+Ended: Feb 07, 2025 @ 5:00am ET
+"""
