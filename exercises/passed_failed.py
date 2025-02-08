785f6c192e2ff222fb6342441b970e4ce3cbf8df
jasonmpittman <jason@jasonmpittman.com>
2025-02-09 10:05:50 +1030

    removed bad str assignment

--- exercises/passed_failed.py
+++ exercises/passed_failed.py
@@ -6,24 +6,46 @@ __version__ = "1.0.0"
 __maintainer__ = "Jason M. Pittman"
 __status__ = "Exercise"
 
-""" Zero Sum: 
-Given an integer array, check if it contains a contiguous subarray having zero-sum.
+""" PassedFailed:
 
-Input : [3, 4, -7, 3, 1, 3, 1, -4, -2, -2]
-Output: True
-Explanation: The subarrays with zero-sum are
+The challenge is to fix all of the bugs in this incredibly messy code, which the code in the image might've actually looked like (probably not)! The code given will output the same middle two lines as in the image shown above.
 
-[3, 4, -7]
-[4, -7, 3]
-[-7, 3, 1, 3]
-[3, 1, -4]
-[3, 1, 3, 1, -4, -2, -2]
-[3, 4, -7, 3, 1, 3, 1, -4, -2, -2]
+First parameter is the user's score.
 
-Input : [4, -7, 1, -2, -1]
-Output: False
+Second parameter is the required score.
+
+    Examples
+
+    grade_percentage("85%", "85%") ➞ "You PASSED the Exam"
+
+    grade_percentage("99%", "85%") ➞ "You PASSED the Exam"
+
+    grade_percentage("65%", "90%") ➞ "You FAILED the Exam"
+
 
 Started: Feb 08, 2025 @ 4:00am ET
 Intervals: 1
 Ended: Feb 08, 2025 @ 4:30am ET
 """
+
+from os import wait
+import sys
+
+def grade_percentage(user_score, pass_score):
+    s=''
+
+    if int(user_score[:-1]) <= int(pass_score[:-1]):
+        s = 'FAILED'
+	
+    if int(user_score[:-1]) >= int(pass_score[:-1]):
+        s = 'PASSED'
+	
+    return 'You'+' '+s+' '+'the Exam'
+
+
+
+if __name__ == "__main__":
+    user_score, pass_score = sys.argv[1], sys.argv[2]
+    result = grade_percentage(user_score, pass_score)
+
+    print(result)
