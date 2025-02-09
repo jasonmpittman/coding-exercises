0f3ba4779b72715179a5b42e8e7ca5e79735cddd
jasonmpittman <jason@jasonmpittman.com>
2025-02-10 05:40:29 +1030

    initial commit

---
+++ exercises/bird_codes.py
@@ -0,0 +1,17 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Bird Codes:
+
+
+
+Started: Feb 10, 2025 @ 5:40am ET
+Intervals: 1
+Ended: Feb 10, 2025 @ 6:10am ET
+"""
+
