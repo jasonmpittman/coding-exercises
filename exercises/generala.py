95a89cba3278d768ba93d85543a4849bc0396491
jasonmpittman <jason@jasonmpittman.com>
2025-02-11 03:16:57 +1030

    initial commit

---
+++ exercises/generala.py
@@ -0,0 +1,15 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Generala:
+
+
+Started: Feb 11, 2025 @ 3:20am ET
+Intervals: 1
+Ended: Feb 11, 2025 @ 3:50am ET
+"""
