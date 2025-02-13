579e2e0932ac9316119ce014b605ccfe43ac3575
jasonmpittman <jason@jasonmpittman.com>
2025-02-14 05:18:33 +1030

    initial commit

---
+++ exercises/string_merge.py
@@ -0,0 +1,21 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" String Merge:
+Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+    Examples
+
+    ("hello", "world", "l")       ==>  "held"
+    ("coding", "anywhere", "n")   ==>  "codinywhere"
+    ("jason", "samson", "s")      ==>  "jasamson"
+    ("wonderful", "people", "e")  ==>  "wondeople"
+
+Started: Feb 14th, 2025 @ 5:20am ET
+Intervals: 1
+Ended: Feb 14th, 2025 @ 5:50am ET
+"""
