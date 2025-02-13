b0e0ad372f75b1992a252e4f6a3754b2dc068bd6
jasonmpittman <jason@jasonmpittman.com>
2025-02-14 05:32:24 +1030

    implemented string merge function

--- exercises/string_merge.py
+++ exercises/string_merge.py
@@ -17,5 +17,30 @@ Given two words and a letter, return a single word that's a combination of both 
 
 Started: Feb 14th, 2025 @ 5:20am ET
 Intervals: 1
-Ended: Feb 14th, 2025 @ 5:50am ET
+Ended: Feb 14th, 2025 @ 5:31am ET
 """
+
+def merge_strings(strings: list) -> str:
+    merged_string = ''
+    first_string, second_string, join_letter = strings[0], strings[1], strings[2]
+    
+    # get position of split letter so we can slice there
+    first_index = first_string.index(join_letter)
+    second_index = second_string.index(join_letter)
+
+    # slice them up
+    first_string = first_string[:first_index]
+    second_string = second_string[second_index + 1:]
+    
+    # merge them
+    merged_string = first_string + join_letter + second_string
+
+    return merged_string
+
+
+if __name__ == "__main__":
+    #strings = ["hello", "world", "l"]
+    strings = ["coding", "anywhere", "n"]
+    
+    merged_string = merge_strings(strings)
+    print(merged_string)
