42ae8a15c88d88f233ceea5ed3a258d7136bfa19
jasonmpittman <jason@jasonmpittman.com>
2025-02-16 19:14:28 +1030

    initial commit

---
+++ exercises/disemvowel_trolls.py
@@ -0,0 +1,22 @@
+
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Disemvowel Trolls:
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Started: Feb 16, 2025 @ 7:15pm ET
+Intervals: 1
+Ended: Feb 16, 2025 @ 7:45am ET
+"""
