7f2dded41995bfa589ad5cf8d2caef0c6ff5eb10
jasonmpittman <jason@jasonmpittman.com>
2025-02-17 07:24:38 +1030

    initial commit

---
+++ exercises/string_average.py
@@ -0,0 +1,19 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" String Average:
+You are given a string of numbers between 0-9. Find the average of these numbers and return it as a floored whole number (ie: no decimal places) written out as a string. Eg:
+
+"zero nine five two" -> "four"
+
+If the string is empty or includes a number greater than 9, return "n/a"
+
+Started: Feb 17, 2025 @ 7:25am ET
+Intervals: 1
+Ended: Feb 17, 2025 @ 7:55am ET
+"""
