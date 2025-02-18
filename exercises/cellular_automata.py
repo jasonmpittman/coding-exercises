0e75479ee088e7c09e3eddf21f69bab14f085a65
jasonmpittman <jason@jasonmpittman.com>
2025-02-19 01:57:31 +1030

    initial commit

---
+++ exercises/cellular_automata.py
@@ -0,0 +1,24 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Cellular Automata:
+
+Part One:
+Started: Feb 19th, 2025 @ 02:00am ET
+Intervals: 1
+Ended: Feb 19th, 2025 @ 2:30am ET
+"""
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
