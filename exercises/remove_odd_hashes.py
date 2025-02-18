ff814bce6968561dcbb1be18ba5b556d1961b06e
jasonmpittman <jason@jasonmpittman.com>
2025-02-18 12:56:37 +1030

    initial commit

---
+++ exercises/remove_odd_hashes.py
@@ -0,0 +1,28 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Remove Odd Hashes:
+
+remove any hash whose two keys sum to an odd number.
+
+example:
+
+remove_odd_hashes(
+  [ {a: 5, b: 5}, 
+    {a: 3, b: 4}, 
+    {a: 2, b: 0}, 
+    {a: 2, b: 1}], 
+  :a, :b)
+  
+# returns [{a: 5, b: 5}, {a: 2, b: 0}]
+
+
+Started: Feb 18th, 2025 @ 12:55pm ET
+Intervals: 1
+Ended: Feb 18th, 2025 @ 1:25pm ET
+"""
