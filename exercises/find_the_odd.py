7816ba815e9c19ffb495d2df9a826a928221d205
jasonmpittman <jason@jasonmpittman.com>
2025-02-26 16:20:02 +1030

    implemented find the odd count function

--- exercises/find_the_odd.py
+++ exercises/find_the_odd.py
@@ -20,7 +20,27 @@ There will always be only one integer that appears an odd number of times.
     [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
     [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).
 
-Started: Feb 26th, 2025 @ 06:10am ET
+Started: Feb 26th, 2025 @ 4:10pm ET (estimated)
 Intervals: 1
-Ended: Feb 26th, 2025 @ 6:40am ET
+Ended: Feb 26th, 2025 @ 4:19pm ET
 """
+
+def find_the_odd(numbers: list) -> int:
+    unique_numbers = set(numbers)
+    odd_count = ''
+
+    for unique_number in unique_numbers:
+        if numbers.count(unique_number) % 2 != 0:
+            odd_count = unique_number
+
+    return odd_count
+
+if __name__ == "__main__":
+    numbers = [1,2,2,3,3,3,4,3,3,3,2,2,1]
+
+    result = find_the_odd(numbers)
+
+    if result != '':
+        print(result)
+    else:
+        print("Something went wrong")
