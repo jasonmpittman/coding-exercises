3b64feb393936f0d4a9b8a3b485beedfb4fe8d4c
jasonmpittman <jason@jasonmpittman.com>
2025-02-27 05:03:02 +1030

    initial commit

---
+++ exercises/bit_counting.py
@@ -0,0 +1,19 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Bit Counting:
+
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+
+Started: Feb 27th, 2025 @ 05:30am ET
+Intervals: 1
+Ended: Feb 27th, 2025 @ 6:00am ET
+"""
