fbd7818c052483d0cd424fbef31e99bcb29b8639
jasonmpittman <jason@jasonmpittman.com>
2025-02-27 05:09:24 +1030

    implemented conversion and count functions

--- exercises/bit_counting.py
+++ exercises/bit_counting.py
@@ -15,5 +15,19 @@ Example: The binary representation of 1234 is 10011010010, so the function shoul
 
 Started: Feb 27th, 2025 @ 05:30am ET
 Intervals: 1
-Ended: Feb 27th, 2025 @ 6:00am ET
+Ended: Feb 27th, 2025 @ 5:38am ET
 """
+import sys
+
+def count_bits(bits: str) -> int:
+    return bits.count('1')
+
+def convert_to_bits(number: int) -> str:
+    return format(number, '08b')
+
+if __name__ == "__main__":
+    number = int(sys.argv[1])
+    bits = convert_to_bits(number)
+    count = count_bits(bits)
+
+    print(count)
