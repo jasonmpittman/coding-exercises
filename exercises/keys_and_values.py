13c34c3eae5a95d6ea54797ab958a6f0b9baeef6
jasonmpittman <jason@jasonmpittman.com>
2025-02-28 06:27:06 +1030

    initial commit

---
+++ exercises/keys_and_values.py
@@ -0,0 +1,26 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Keys and Values:
+Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+
+Example:
+
+keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]]
+
+Style Points (JS/CoffeeScript only): This kata only tests for data that uses object literal notation (simple objects). For extra style, can you get your method to check for objects that extend their prototype?
+
+
+Started: Feb 28th, 2025 @ 6:25am ET (estimated)
+Intervals: 1
+Ended: Feb 28th, 2025 @ 6:55am ET
+"""
+
+
+
+
