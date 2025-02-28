6399865c3f5148ee6e29cd4524ad7541b0aa303d
jasonmpittman <jason@jasonmpittman.com>
2025-03-01 07:34:42 +1030

    implemented num to word map

--- exercises/digit_by_digit.py
+++ exercises/digit_by_digit.py
@@ -16,5 +16,45 @@ Example:
 
 Started: Mar 1st, 2025 @ 7:15am ET (estimated)
 Intervals: 1
-Ended: Mar 1st, 2025 @ 7:45am ET
+Ended: Mar 1st, 2025 @ 7:34am ET
 """
+import sys
+
+word_map = {
+    'zero': 0,
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9,
+    }
+
+number_map = {
+    0: 'zero',
+    1: 'one',
+    2: 'two',
+    3: 'three',
+    4: 'four',
+    5: 'five',
+    6: 'six',
+    7: 'seven',
+    8: 'eight',
+    9: 'nine'
+}
+
+
+def convert_int_to_words(number: str) -> str:
+    converted_number = 'Num'
+
+    for n in number:
+        converted_number = converted_number + '.' + number_map[int(n)]
+
+    print(converted_number)
+
+if __name__ == "__main__":
+    number = sys.argv[1]
+    convert_int_to_words(number)
