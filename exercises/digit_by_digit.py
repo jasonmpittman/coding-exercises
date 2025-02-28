c22808f30688f0a7326477fa68822da22833c736
jasonmpittman <jason@jasonmpittman.com>
2025-03-01 07:15:29 +1030

    initial commit

---
+++ exercises/digit_by_digit.py
@@ -0,0 +1,20 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Digit by Digit Number:
+Implement a class that allows chaining methods zero, one, two, three, four, five, six, seven, eight, nine to construct numbers in a similar manner.
+
+Note: your class should also support a method which converts its instances into integers (provided in the initial solution).
+
+Example:
+    731 you could write fancy Num.seven.three.one!
+
+Started: Mar 1st, 2025 @ 7:15am ET (estimated)
+Intervals: 1
+Ended: Mar 1st, 2025 @ 7:45am ET
+"""
