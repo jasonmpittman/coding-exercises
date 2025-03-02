42ba22102963c20cd98c555995e0c37e0c8cffa7
jasonmpittman <jason@jasonmpittman.com>
2025-03-03 07:43:04 +1030

    initial commit

---
+++ exercises/isograms.py
@@ -0,0 +1,21 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Isograms: 
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+    Example: (Input --> Output)
+
+    "Dermatoglyphics" --> true
+    "aba" --> false
+    "moOse" --> false (ignore letter case)
+
+Started: Mar 3rd, 2025 @ 7:40am ET (estimated)
+Intervals: 1
+Ended: Mar 3rd, 2025 @ 8:10am ET
+"""
