5e638b87e7298e64d1b49c09e691fba9d003cfa7
jasonmpittman <jason@jasonmpittman.com>
2025-03-04 09:01:48 +1030

    initial commit

---
+++ exercises/flatten_arrays.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Flatten Arrays:
+
+write an algorithm that flatten an arrays structure to 2 levels. It means that each array under the second level will be merge to its parent. Only two levels are kept.
+
+Empty arrays are ignored.
+
+array = [1, [2, 3], [4, 5, [6, 7, 8], 9, 10, [11, [12, [13, 14], 15], 16], 17], 18];
+return [1,[2,3],[4,5,6,7,8,9,10,11,12,13,14,15,16,17], 18]
+
+array = [1,[2, 3, [], [4, [], 5]]]
+return [1,[2,3,4,5]]
+
+
+Started: Mar 4th, 2025 @ 9:00am ET (estimated)
+Intervals: 1
+Ended: Mar 4th, 2025 @ 9:30am ET
+"""
