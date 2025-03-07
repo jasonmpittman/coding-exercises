cd69d604d506dd3be84464c0d081fcda2bc1d1ad
jasonmpittman <jason@jasonmpittman.com>
2025-03-08 09:04:01 +1030

    implemented decoding function

--- exercises/roman_numeral_decoder.py
+++ exercises/roman_numeral_decoder.py
@@ -20,7 +20,40 @@ Modern Roman numerals are written by expressing each decimal digit of the number
     "XL"      ->   40
     "I"       ->    1
 
-Started: Mar 8th, 2025 @ 8:55am ET (estimated)
+Started: Mar 8th, 2025 @ 8:52am ET (estimated)
 Intervals: 1
-Ended: Mar 8th, 2025 @ 6:28am ET
+Ended: Mar 8th, 2025 @ 9:03am ET
 """
+import sys
+
+def convert_roman_to_integer(numberals):
+    roman_numbers = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for r in reversed(numberals):  # Iterate through the string in reverse order
+        value = roman_numbers[r]
+        if value < prev_value:
+            # If the current value is less than the previous value, subtract it
+            total -= value
+        else:
+            # Otherwise, add it to the total
+            total += value
+            prev_value = value
+
+    return total
+
+if __name__ == "__main__":
+    numerals = sys.argv[1]
+
+    decoded_value = convert_roman_to_integer(numerals)
+    print(decoded_value)
