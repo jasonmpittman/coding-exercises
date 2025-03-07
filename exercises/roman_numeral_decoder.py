835cd95bc2a264fae74f38adea71a5dd340ca2a6
jasonmpittman <jason@jasonmpittman.com>
2025-03-08 08:51:46 +1030

    initial commit

---
+++ exercises/roman_numeral_decoder.py
@@ -0,0 +1,26 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Roman Number Decoder:
+Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+    Example:
+
+    "MM"      -> 2000
+    "MDCLXVI" -> 1666
+    "M"       -> 1000
+    "CD"      ->  400
+    "XC"      ->   90
+    "XL"      ->   40
+    "I"       ->    1
+
+Started: Mar 8th, 2025 @ 8:55am ET (estimated)
+Intervals: 1
+Ended: Mar 8th, 2025 @ 6:28am ET
+"""
