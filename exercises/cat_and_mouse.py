e44888d062514a0443cfa01617f05df72a527b4a
jasonmpittman <jason@jasonmpittman.com>
2025-03-09 06:55:14 +1030

    initial commit

---
+++ exercises/cat_and_mouse.py
@@ -0,0 +1,23 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Cat and Mouse:
+
+You will be given a string featuring a cat 'C' and a mouse 'm'. The rest of the string will be made up of '.'. The string will start with the cat, and end with the mouse.
+
+You need to find out if the cat can catch the mouse from its current position. The cat can jump over at most three characters. So:
+
+"C.....m" returns "Escaped!" <-- more than three characters between
+
+"C...m" returns "Caught!" <-- as there are three characters between the two, the cat can jump.
+
+
+Started: Mar 9th, 2025 @ 6:55am ET (estimated)
+Intervals: 1
+Ended: Mar 9th, 2025 @ 7:25am ET
+"""
