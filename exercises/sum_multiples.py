dc403f76f66682708f6834c61a5e1eb985569774
jasonmpittman <jason@jasonmpittman.com>
2025-03-10 06:20:54 +1030

    initial commit

---
+++ exercises/sum_multiples.py
@@ -0,0 +1,16 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Sum Multiples:
+
+Return the sum of the multiples of 3 and 5 below a number
+
+Started: Mar 10th, 2025 @ 6:20am ET
+Intervals: 1
+Ended: Mar 7th, 2025 @ 6:50am ET
+"""
