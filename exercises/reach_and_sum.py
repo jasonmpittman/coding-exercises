68a8515d0b62e848145333cb3f4c9ffd4cbd3e73
jasonmpittman <jason@jasonmpittman.com>
2025-03-11 06:07:36 +1030

    initial commit

---
+++ exercises/reach_and_sum.py
@@ -0,0 +1,29 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+
+""" Reach and Sum:
+Make a function that receives three arguments in this order:
+
+    init_val (initVal) is the starting number;
+    pattern_l (patternL) is the list of repeating differences;
+    nth_term (nthTerm) is the ordinal position of the term in the sequence (≤ 1,500,000).
+
+The function should return the sum of the digits of the nth_term in the sequence.
+Example Cases:
+
+sum_dig_nth_term(10, [2, 1, 3], 6) → 10  
+# 6th term = 19 → sum of digits = 1 + 9 = 10  
+
+sum_dig_nth_term(10, [1, 2, 3], 15) → 10  
+# 15th term = 37 → sum of digits = 3 + 7 = 10  
+
+
+Started: Mar 11th, 2025 @ 6:10am ET
+Intervals: 1
+Ended: Mar 11th, 2025 @ 6:40am ET
+"""
