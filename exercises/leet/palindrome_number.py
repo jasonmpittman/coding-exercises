7083178688f80da43cce9861c88f6fd3f58df0fe
jasonmpittman <jason@jasonmpittman.com>
2025-07-23 04:21:56 -0400

    initial commit

---
+++ exercises/leet/palindrome_number.py
@@ -0,0 +1,36 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = "Easy"
+
+"""
+Given an integer x, return true if x is a, and false otherwise. 
+
+Examples:
+    Input: x = 121
+    Output: true
+    Explanation: 121 reads as 121 from left to right and from right to left.
+
+    Input: x = -121
+    Output: false
+    Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+
+    Input: x = 10
+    Output: false
+    Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+Start:
+End:
+"""
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
