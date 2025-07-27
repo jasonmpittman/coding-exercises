5bbfb65ebda1d69d03694586259abc25ffcc49ab
jasonmpittman <jason@jasonmpittman.com>
2025-07-27 07:59:04 -0400

    initial commit

---
+++ exercises/leet/remove_nth.py
@@ -0,0 +1,37 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = "Medium"
+
+"""
+Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+Example 1:
+    Input: head = [1,2,3,4,5], n = 2
+    Output: [1,2,3,5]
+
+Example 2:
+    Input: head = [1], n = 1
+    Output: []
+
+Example 3:
+    Input: head = [1,2], n = 1
+    Output: [1]
+
+
+Start: 8:00am
+End: 
+"""
+
+from sys import argv
+
+def main()
+    pass
+
+
+if __name__ == "__main__":
+    pass
