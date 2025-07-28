7d57661664f8e3e300f751345bfe0acdcdcac26a
jasonmpittman <jason@jasonmpittman.com>
2025-07-28 08:19:25 -0400

    initial commit

---
+++ exercises/leet/parentheses_generator.py
@@ -0,0 +1,31 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = "Medium"
+
+"""
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+    Input: n = 3
+    Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Example 2:
+    Input: n = 1
+    Output: ["()"]
+
+Start: 8:20am
+End:
+"""
+
+def main():
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
