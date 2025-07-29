9212e713dfdbde2530762c5136e57e6ce32f82d0
jasonmpittman <jason@jasonmpittman.com>
2025-07-29 03:00:14 -0400

    initial commit

---
+++ exercises/ccwc.py
@@ -0,0 +1,28 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+ccwc - a 'wc' clone
+
+part 1: clone -c argument
+part 2: clone -l argument
+part 3: clone -w argument
+part 4: clone -m argument
+part 5: clone default as -clw
+part 6: clone read from stdout as input
+
+Start: 7.29.25 3:00am
+End:
+Cycles: 1
+"""
+
+
+
+if __name__ == "__main__":
+    pass
