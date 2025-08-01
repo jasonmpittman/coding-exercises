2b2d758c4ca91e4ce4184000397fd4913d00f31e
jasonmpittman <jason@jasonmpittman.com>
2025-08-01 05:52:38 -0400

    completed -l implementation

--- exercises/ccwc.py
+++ exercises/ccwc.py
@@ -10,14 +10,14 @@ __difficuluty__ = ""
 """
 ccwc - a 'wc' clone
 
-part 1: clone -c argument
+part 1: clone -c argument (19 minutes)
 part 2: clone -l argument
 part 3: clone -w argument
 part 4: clone -m argument
 part 5: clone default as -clw
 part 6: clone read from stdout as input
 
-Start: 7.29.25 3:00am
+Start: 7.29.25 3:00am, 8.01.25 5:45am
 End:
 Cycles: 1
 """
@@ -35,12 +35,29 @@ def get_file_bytes(file: str) -> int:
 
     return number_of_bytes
 
+def get_number_of_lines(file: str) -> int:
+    """ Returns the number of lines in each input file is written to the standard output. """
+    number_of_lines = 0
+
+    try:
+        with open(file, 'r') as f:
+            number_of_lines = len(f.readlines())
+    except Exception as e:
+        print(f'An error occured while attempting to access the file: {e}')
+
+    return number_of_lines
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='A Python clone of the wc utility')
     parser.add_argument('-c', help='The number of bytes in each input file')
+    parser.add_argument('-l', help='The number of lines in each input file')
 
     args = vars(parser.parse_args())
 
     if args['c']:
         result = get_file_bytes(args['c'])
         print('    ' + str(result) + ' ' + args['c'])
+    
+    if args['l']:
+        result = get_number_of_lines(args['l'])
+        print('    ' + str(result) + ' ' + args['l'])
