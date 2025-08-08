d4edfc19c0e97f04b0bea15862fd85d1ef43d551
jasonmpittman <jason@jasonmpittman.com>
2025-08-08 06:21:45 -0400

    initial commit

---
+++ exercises/redis-server/server.py
@@ -0,0 +1,30 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+import socket
+
+class Redis:
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address = ('0.0.0.0', 6379)
+        self.sock.bind(self.server_address)
+
+    def run(self):
+        self.sock.listen(1)
+
+        while True:
+            print(f'Waiting for a connection')
+            connection, client_address = self.sock.accept()
+
+
+
+
+if __name__ == "__main__":
+    redis = Redis()
+    redis.run()
