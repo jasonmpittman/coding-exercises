5a6d3e0e7f242bce57040d9881d5ff5627c05c67
jasonmpittman <jason@jasonmpittman.com>
2025-08-10 07:13:43 -0400

    initial commit

---
+++ exercises/redis-server/client.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Start: 8.10.25 7:10am
+End:
+Cycles: 1
+"""
+import socket
+
+class RedisClient:
+    def __init__(self):
+        pass
+
+
+
+
+if __name__ == "__main__":
+    pass
