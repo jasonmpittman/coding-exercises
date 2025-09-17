48f8cfa8694da0374c114b5ab26f62565a0b4dcb
jasonmpittman <jason@jasonmpittman.com>
2025-09-17 08:47:15 -0400

    initial commit

---
+++ exercises/diamond_array.py
@@ -0,0 +1,29 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Create a function that returns an array that expands by 1 from 1 to the value of the input, and then reduces back to 1. Items in the arrays will be the same as the length of the arrays.
+
+Examples
+    diamondArrays(1) ➞ [[1]]
+    diamondArrays(2) ➞ [[1], [2, 2], [1]]
+    diamondArrays(5) ➞ [[1], [2, 2], [3, 3, 3], [4, 4, 4, 4], [5, 5, 5, 5, 5], [4, 4, 4, 4], [3, 3, 3], [2, 2], [1]]
+
+Start: 8:45am
+End: am
+Cycles: 1
+"""
+import sys
+
+def create_diamond_array(input: int) -> list:
+    pass
+
+if __name__ == "__main__":
+    input = int(sys.argv[1])
+    create_diamond_array(input)
