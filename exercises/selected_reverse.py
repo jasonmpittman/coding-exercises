774c643597ef27ee7cd1b44f39c393245e8bea76
jasonmpittman <jason@jasonmpittman.com>
2025-09-18 05:26:11 -0400

    initial commit

---
+++ exercises/selected_reverse.py
@@ -0,0 +1,27 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Create a function that takes a list and a number and selectively reverse the order of the list based on the number you're given (second argument). If you're given the arguments [1,2,3,4,5,6] and 2, you would return the list [2,1, 4,3, 6,5].
+
+Examples
+    sel_reverse([1,2,3,4,5,6], 2) ➞ [2,1, 4,3, 6,5]
+    sel_reverse([2,4,6,8,10,12,14,16], 3) ➞ [6,4,2, 12,10,8, 16,14]
+    sel_reverse([5,7,2,6,0,4,6], 100) ➞ [6,4,0,6,2,7,5]
+
+Start: 5:25am
+End: :am
+Cycles: 1
+"""
+
+def reverse_selection(array: list, n: int) -> list:
+    pass
+
+if __name__ == "__main__":
+    pass
