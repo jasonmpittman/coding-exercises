1cfc2264fcab702fbfcaba95cf274b2aea3d3d0d
jasonmpittman <jason@jasonmpittman.com>
2025-09-20 04:59:12 -0400

    initial commit

---
+++ exercises/fibonacci_string.py
@@ -0,0 +1,29 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+A Fibonacci string is a precedence of the Fibonacci series. It works with any two characters of the English alphabet (as opposed to the numbers 0 and 1 in the Fibonacci series) as the initial items and concatenates them together as it progresses in a similar fashion as the Fibonacci series.
+Examples
+
+fib_str(3, ["j", "h"]) ➞ "j, h, hj"
+
+fib_str(5, ["e", "a"]) ➞ "e, a, ae, aea, aeaae"
+
+fib_str(6, ["n", "k"]) ➞ "n, k, kn, knk, knkkn, knkknknk"
+
+Start: 5:00am
+End: 5:30:am
+Cycles: 1
+"""
+import sys
+
+
+
+if __name__ == "__main__":
+    pass
