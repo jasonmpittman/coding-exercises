9d610c62174646df4d914dcd98df3478850fa757
jasonmpittman <jason@jasonmpittman.com>
2025-09-22 05:56:33 -0400

    initial commit

---
+++ exercises/begin_end_pair.py
@@ -0,0 +1,22 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+write a function that pairs the first number in an array with the last, the second number with the second to last, etc.
+
+Examples
+    pairs([1, 2, 3, 4, 5, 6, 7]) ➞ [[1, 7], [2, 6], [3, 5], [4, 4]]
+    pairs([1, 2, 3, 4, 5, 6]) ➞ [[1, 6], [2, 5], [3, 4]]
+    pairs([5, 9, 8, 1, 2]) ➞ [[5, 2], [9, 1], [8, 8]]
+    pairs([]) ➞ []
+
+Start: 5:55am
+End: 6:25:am
+Cycles: 1
+"""
