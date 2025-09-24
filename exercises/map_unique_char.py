37bae7f1b1991530a9d5544a27cbd87552c2ab61
jasonmpittman <jason@jasonmpittman.com>
2025-09-24 06:25:29 -0400

    initial commit

---
+++ exercises/map_unique_char.py
@@ -0,0 +1,30 @@
+
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Write a function that returns a character mapping from a word.
+
+Examples:
+    character_mapping("abcd") ➞ [0, 1, 2, 3]
+    character_mapping("abb") ➞ [0, 1, 1]
+    character_mapping("babbcb") ➞ [0, 1, 0, 0, 2, 0]
+    character_mapping("hmmmmm") ➞ [0, 1, 1, 1, 1, 1]
+
+Start: 6:25am
+End: 6:55:am
+Cycles: 1
+"""
+from sys import argv[1]
+
+
+
+if __name__ == "__main__":
+    pass
+
