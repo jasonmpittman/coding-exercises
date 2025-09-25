3e96784748a69cb7d7a3825305cd36691164f304
jasonmpittman <jason@jasonmpittman.com>
2025-09-25 05:09:48 -0400

    initial commit

---
+++ exercises/nested_list_length.py
@@ -0,0 +1,30 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Python's len() method will return the number of elements in a list. For example, the list below contains 2 elements:
+
+[1, [2, 3]]
+# 2 elements, number 1 and list [2, 3]
+
+Suppose we instead wanted to know the total number of non-nested items in the nested list. In the above case, [1, [2, 3]] contains 3 non-nested items, 1, 2 and 3.
+
+Create a function that returns the total number of non-nested items in a nested list.
+
+Examples
+    get_length([1, [2, 3]]) ➞ 3
+    get_length([1, [2, [3, 4]]]) ➞ 4
+    get_length([1, [2, [3, [4, [5, 6]]]]]) ➞ 6
+    get_length([1, [2], 1, [2], 1]) ➞ 5
+
+Start: 5:10am
+End: 5:40:am
+Cycles: 1
+"""
+
