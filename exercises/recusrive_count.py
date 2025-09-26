cc5cc110855066ec6c6f1a57e3889bde4a39ac97
jasonmpittman <jason@jasonmpittman.com>
2025-09-26 05:40:47 -0400

    initial commit

---
+++ exercises/recusrive_count.py
@@ -0,0 +1,23 @@
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Create a function that recursively counts the integer's number of digits.
+
+Examples
+    count(318) ➞ 3
+    count(-92563) ➞ 5
+    count(4666) ➞ 4
+    count(-314890) ➞ 6
+    count(654321) ➞ 6
+    count(638476) ➞ 6
+
+Start: 5:40am
+End: 6:20:am
+Cycles: 1
+"""
+
+
