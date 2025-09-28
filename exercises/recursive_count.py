062439ed80b8aebcf0a18dc7a54a41dd9c2d76bb
jasonmpittman <jason@jasonmpittman.com>
2025-09-28 00:44:38 -0400

    fixed yaml header

--- exercises/recursive_count.py
+++ exercises/recursive_count.py
@@ -1,3 +1,6 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
 __license__ = "MIT License"
 __version__ = "1.0.0"
 __maintainer__ = "Jason M. Pittman"
