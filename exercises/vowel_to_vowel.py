114b10ae1850663bb3c08743bbf544f7d197f269
jasonmpittman <jason@jasonmpittman.com>
2025-09-29 06:49:03 -0400

    initial commit

---
+++ exercises/vowel_to_vowel.py
@@ -0,0 +1,27 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Given a sentence as txt, return True if any two adjacent words have this property: One word ends with a vowel, while the word immediately after begins with a vowel (a e i o u).
+Examples
+
+vowel_links("a very large appliance") ➞ True
+
+vowel_links("go to edabit") ➞ True
+
+vowel_links("an open fire") ➞ False
+
+vowel_links("a sudden applause") ➞ False
+
+Start: 06:50am
+End: 07:20:am
+Cycles: 1
+"""
+
+
