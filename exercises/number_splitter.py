e08690bf722e09374136f844c0d81b08ab90bf0a
jasonmpittman <jason@jasonmpittman.com>
2025-09-30 04:44:14 -0400

    initial commit

---
+++ exercises/number_splitter.py
@@ -0,0 +1,27 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Given a number, return a list containing the two halves of the number. If the number is odd, make the rightmost number higher.
+
+Examples
+    number_split(4) ➞ [2, 2]
+    number_split(10) ➞ [5, 5]
+    number_split(11) ➞ [5, 6]
+    number_split(-9) ➞ [-5, -4]
+
+Start: 04:45am
+End: 05:05:am
+Cycles: 1
+"""
+from sys import argv[1]
+
+
+if __name__ == "__main__":
+    pass
