2bb615de91e040b7a951d42b97d95a2fd00db0ec
jasonmpittman <jason@jasonmpittman.com>
2025-10-01 05:37:27 -0400

    implemented operations and unjumble functions

--- exercises/jumbled_equations.py
+++ exercises/jumbled_equations.py
@@ -19,8 +19,34 @@ Examples
     # "1+0=1" or "3-3=0" are not allowed.
 
 Start: 05:15am
-End: 05:45:am
+End: 05:36:am
 Cycles: 1
 """
+import operator
+import random
 
+operations = {
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv
+}
+
+def unjumbled_equation(operators: list, values: list):
+    equations = []
+    values.sort()
+
+    for operator in operators:
+        selected_values = random.sample(values, 2)
+        result = operations[operator](selected_values[0], selected_values[1])
+        print(f'{selected_values[0]} {operator} {selected_values[1]} = {result}')
+
+    return equations
+
+if __name__ == "__main__":
+    equation_parts = [["+", "*"], [36, 28, 71, 4, 12, 7, 11]] #[["+"], [2, 1, 3]]
+    operators = equation_parts[0] 
+    values = equation_parts[1]
+
+    unjumbled_equation(operators, values)
 
