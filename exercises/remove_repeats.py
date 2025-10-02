61adb19b44f9d0177508197e58efd909234f2122
jasonmpittman <jason@jasonmpittman.com>
2025-10-02 05:51:58 -0400

    initial commit

---
+++ exercises/remove_repeats.py
@@ -0,0 +1,24 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Create a function that will remove any repeated character(s) in a word passed to the function. Not just consecutive characters, but characters repeating anywhere in the input.
+
+Examples
+    unrepeated("hello") ➞ "helo"
+    unrepeated("aaaaa") ➞ "a"
+    unrepeated("WWE!!!") ➞ "WE!"
+    unrepeated("call 911") ➞ "cal 91"
+
+Start: 05:55am
+End: 06:25:am
+Cycles: 1
+"""
+
+
