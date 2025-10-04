7dca116c8b9c8a4977eeb94a59a713594af41897
jasonmpittman <jason@jasonmpittman.com>
2025-10-04 10:24:51 -0400

    initial commit

---
+++ exercises/string_stretched.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Write a function that returns True if the first string is the second string stretched, and False otherwise. A stretch is to repeat each character in a string the same number of times.
+
+Examples
+    is_stretched("pppaaannndddaaa", "panda") ➞ True
+    is_stretched("sssshhiipp", "ship") ➞ False
+    is_stretched("magnet", "magnet") ➞ True
+    is_stretched("magneto", "magnet") ➞ False
+
+Start: 10:25am
+End: 10:55:am
+Cycles: 1
+"""
+
+
+
