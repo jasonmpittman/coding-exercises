cce07b4843207ca6cd93c20afbe3526f90068ba8
jasonmpittman <jason@jasonmpittman.com>
2025-10-09 04:40:15 -0400

    initial commit

---
+++ exercises/split_numbers.py
@@ -0,0 +1,21 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Create a function that takes a number num and returns each place value in the number.
+
+Examples
+    num_split(39) ➞ [30, 9]
+    num_split(-434) ➞ [-400, -30, -4]
+    num_split(100) ➞ [100, 0, 0]
+
+Start: 04:40am
+End: 05:00:am
+Cycles: 1
+"""
