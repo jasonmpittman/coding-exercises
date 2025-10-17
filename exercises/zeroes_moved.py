39eb31670f12b3e0e67665bbf80bbac2d4a157bc
jasonmpittman <jason@jasonmpittman.com>
2025-10-17 03:19:47 -0400

    initial commit

---
+++ exercises/zeroes_moved.py
@@ -0,0 +1,28 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+Start: 03:20am
+End: 03:50:am
+Cycles: 1
+"""
+from sys import argv
+
+
+
+if __name__ == "__main__":
+    array = argv[1].split(',')
+
+
+
+
