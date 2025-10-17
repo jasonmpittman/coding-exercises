8414cf3fe57ee1088553bd508d369958d7f23bc1
jasonmpittman <jason@jasonmpittman.com>
2025-10-17 03:25:14 -0400

    implemented move zeroes function

--- exercises/zeroes_moved.py
+++ exercises/zeroes_moved.py
@@ -13,15 +13,29 @@ Write an algorithm that takes an array and moves all of the zeros to the end, pr
 moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
 
 Start: 03:20am
-End: 03:50:am
+End: 03:24:am
 Cycles: 1
 """
 from sys import argv
 
+def move_zeroes(array: list) -> list:
+    moved_zeroes = []
+    index = 0
 
+    for element in array:
+        if element == '0':
+            moved_zeroes.append(element)
+        else:
+            moved_zeroes.insert(index, element)
+            index += 1
+    
+    return moved_zeroes
 
 if __name__ == "__main__":
     array = argv[1].split(',')
+
+    result = move_zeroes(array)
+    print(result)
 
 
 
