2c79154d19614dd2e07ef0e04617be252f2c5324
jasonmpittman <jason@jasonmpittman.com>
2025-10-24 07:00:05 -0400

    initial commit

---
+++ exercises/encoded_string_parse.py
@@ -0,0 +1,37 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Create a function which takes in an encoded string and returns a dictionary according to the following example:
+Examples
+
+parse_code("John000Doe000123") ➞ {
+  "first_name": "John",
+  "last_name": "Doe",
+  "id": "123"
+}
+
+parse_code("michael0smith004331") ➞ {
+  "first_name": "michael",
+  "last_name": "smith",
+  "id": "4331"
+}
+
+parse_code("Thomas00LEE0000043") ➞ {
+  "first_name": "Thomas",
+  "last_name": "LEE",
+  "id": "43"
+}
+
+Start: 07:00am
+End: 07:30am
+Cycles: 1
+"""
+
+
