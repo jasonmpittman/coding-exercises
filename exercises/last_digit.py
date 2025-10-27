abc6ecccac2fce47ec5b581192a2487b9db19f95
jasonmpittman <jason@jasonmpittman.com>
2025-10-27 07:11:07 -0400

    initial commit

---
+++ exercises/last_digit.py
@@ -0,0 +1,31 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Define a function that takes in two non-negative integers aaa and bbb and returns the last decimal digit of aba^bab. Note that aaa and bbb may be very large!
+
+For example, the last decimal digit of 979^797 is 999, since 97=47829699^7 = 478296997=4782969. The last decimal digit of (2200)2300({2^{200}})^{2^{300}}(2200)2300, which has over 109210^{92}1092 decimal digits, is 666. Also, please take 000^000 to be 111.
+
+You may assume that the input will always be valid.
+
+Examples
+    lastDigit 4 1             `shouldBe` 4
+    lastDigit 4 2             `shouldBe` 6
+    lastDigit 9 7             `shouldBe` 9
+    lastDigit 10 (10^10)      `shouldBe` 0
+    lastDigit (2^200) (2^300) `shouldBe` 6
+
+Start: 07:10pm
+End: 07:40pm
+Cycles: 1
+"""
+
+
+
+
