c593567c51371bbf310bea9c8c5492f2b2dd07f0
jasonmpittman <jason@jasonmpittman.com>
2025-10-28 05:25:56 -0400

    initial commit

---
+++ exercises/duplicate_arguments.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Write a function that returns true if it contains any duplicate argument values, and false otherwise. Any number of arguments may be passed into the function.
+
+The arguments passed in will only be strings or numbers.
+
+Examples:
+    solution(1, 2, 3)             -->  false
+    solution(1, 2, 3, 2)          -->  true
+    solution('1', '2', '3', '2')  -->  true
+
+Start: 05:25pm
+End: 05:55pm
+Cycles: 1
+"""
+
+
