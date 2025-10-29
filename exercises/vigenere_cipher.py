c1b1b7fa71a50c5da0532f2f8a4d8cb4d33000e9
jasonmpittman <jason@jasonmpittman.com>
2025-10-29 07:05:34 -0400

    initial commit

---
+++ exercises/vigenere_cipher.py
@@ -0,0 +1,28 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Write a class that, when given a key and an alphabet, can be used to encode and decode from a Vigenere cipher.
+Examples
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+key      = "password"
+
+"codewars" --> encode -->  "rovwsoiv"
+"laxxhsj"  --> decode -->  "waffles"
+
+Note: any character not in the alphabet must be left alone. For example in the above case:
+
+"CODEWARS"  --> encode -->  "CODEWARS"
+
+
+Start: 07:05pm
+End: 07:35pm
+Cycles: 1
+"""
