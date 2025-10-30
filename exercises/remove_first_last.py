0739563967fa45a95d2f9ea46dcd003c672a3d94
jasonmpittman <jason@jasonmpittman.com>
2025-10-30 10:25:16 -0400

    initial commit

---
+++ exercises/remove_first_last.py
@@ -0,0 +1,28 @@
+
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+write a function that removes the first and last characters of a string. You're given one parameter, the original string.
+
+Important: Your function should handle strings of any length ≥ 2 characters. For strings with exactly 2 characters, return an empty string.
+Examples
+
+'eloquent' --> 'loquen'
+'country'  --> 'ountr' 
+'person'   --> 'erso'
+'ab'       --> '' (empty string)
+'xyz'      --> 'y'
+
+Start: 10:25pm
+End: 10:55pm
+Cycles: 1
+"""
+
+
