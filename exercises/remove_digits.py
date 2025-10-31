a1d46eb44d0a72aaa62cdac58dbe1c627f6a3c64
jasonmpittman <jason@jasonmpittman.com>
2025-10-31 06:49:47 -0400

    initial commit

---
+++ exercises/remove_digits.py
@@ -0,0 +1,19 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Write a function that removes digits 1234567890 from strings. 
+
+Example 
+    "abc123" -> "abc"
+
+Start: 6:50pm
+End: 07:20pm
+Cycles: 1
+"""
