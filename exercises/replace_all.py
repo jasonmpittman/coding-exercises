242a2c721e505cee7380442ae464557a1e4e84b1
jasonmpittman <jason@jasonmpittman.com>
2025-11-02 05:56:08 -0500

    initial commit

---
+++ exercises/replace_all.py
@@ -0,0 +1,22 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+You are given two characters (s, r), and a string (a) terminated with \0.
+
+Replace all occurrences of s in a by r.
+
+Example:
+    Example: replaceAll [1,2,2] 1 2 -> in list [1,2,2] we replace 1 with 2 to get new list [2,2,2]
+
+Start: 5:55pm
+End: 06:20pm
+Cycles: 1
+"""
+
