c1b8d1766c650a49e882fbe6fc93b014dcc106bd
jasonmpittman <jason@jasonmpittman.com>
2025-11-03 08:29:05 -0500

    initial commit

---
+++ exercises/blur_string.py
@@ -0,0 +1,24 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Given a string of length >= 2, repeat every character between the first and last character twice.
+
+Examples
+    blur('af') -> 'af'
+    blur('asf') -> 'assf'
+    blur('asdf') -> 'assddf'
+    blur('fieowg') -> 'fiieeoowwg'
+
+Start: 5:55pm
+End: 06:00pm
+Cycles: 1
+"""
+
+
