eedf09dd58049ee47bd54d216fd6ce0bbd23d76d
jasonmpittman <jason@jasonmpittman.com>
2025-11-04 04:38:21 -0500

    initial commit

---
+++ exercises/string_duplicate_encoder.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Write a function to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+
+Examples
+    "din"      =>  "((("
+    "recede"   =>  "()()()"
+    "Success"  =>  ")())())"
+    "(( @"     =>  "))((" 
+
+Start: 4:40am
+End: 05:10am
+Cycles: 1
+"""
+
+
+
