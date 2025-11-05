daaa31060f8b853b92eac482308c6f4194c52c98
jasonmpittman <jason@jasonmpittman.com>
2025-11-05 06:37:04 -0500

    implemented recursive count function

--- exercises/array_deep_count.py
+++ exercises/array_deep_count.py
@@ -13,13 +13,31 @@ You are given an array. Complete the function that returns the number of ALL ele
 Examples
     []                   -->  0
     [1, 2, 3]            -->  3
-    ["x", "y", ["z"]]    -->  4
-    [1, 2, [3, 4, [5]]]  -->  7 
+    [1,2,[3,4]]          --> 4
+    [1, 2, [3, 4, [5, 6]], 7, [8]] --> 8
 
 Start: 6:20am
 End: 06:50am
 Cycles: 1
 """
+
+def count_deep(array: list) -> int:
+    count = 0
+
+    for element in array:
+        if isinstance(element, list):
+            count += count_deep(element)
+        else:
+            count += 1
+
+    return count
+
+if __name__ == "__main__":
+    array = [1,2,[3,4]] 
+    print(array)
+
+    result = count_deep(array)
+    print(result)
 
 
 
