617cddcbecf65045208ea5e002fcedb52465d921
jasonmpittman <jason@jasonmpittman.com>
2025-11-07 05:06:18 -0500

    initial commit

---
+++ exercises/average_scores.py
@@ -0,0 +1,25 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Create a function that returns the average of an array of numbers ("scores"), rounded to the nearest whole number. You are not allowed to use any loops (including for, for/in, while, and do/while loops).
+
+The array will never be empty.
+
+Examples
+    [2, 452, 5, 8, 92, 46, 3, 69] equal to 676, and the average is 84.5.
+
+Start: 5:05am
+End: 05:35am
+Cycles: 1
+"""
+
+
+
+
