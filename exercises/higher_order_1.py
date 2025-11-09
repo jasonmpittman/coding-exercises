ec8021c221a3c5b47157be452a2f3c5f8527faa8
jasonmpittman <jason@jasonmpittman.com>
2025-11-09 04:29:59 -0500

    initial commit

---
+++ exercises/higher_order_1.py
@@ -0,0 +1,31 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+You will be given an array of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising. The list is ordered according to who signed up first.
+
+Your task is to return one of the following strings:
+
+    < firstName here >, < country here > of the first Python developer who has signed up; or
+    There will be no Python developers if no Python developer has signed up.
+
+For example, given the following input array:
+
+var list1 = [
+  { firstName: 'Mark', lastName: 'G.', country: 'Scotland', continent: 'Europe', age: 22, language: 'JavaScript' },
+  { firstName: 'Victoria', lastName: 'T.', country: 'Puerto Rico', continent: 'Americas', age: 30, language: 'Python' },
+  { firstName: 'Emma', lastName: 'B.', country: 'Norway', continent: 'Europe', age: 19, language: 'Clojure' }
+];
+
+your function should return Victoria, Puerto Rico.
+
+Start: 4:30am
+End: 05:00am
+Cycles: 1
+"""
