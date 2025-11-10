35f4c52bd08d0f7e61e0d7e61c66d050bdded8ae
jasonmpittman <jason@jasonmpittman.com>
2025-11-10 06:21:48 -0500

    initial commit

---
+++ exercises/midpoint_sum.py
@@ -0,0 +1,29 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+For a given list of integers, return the index of the element where the sums of the integers to the left and right of the current element are equal.
+
+Ex:
+
+ints = [4, 1, 7, 9, 3, 9]
+# Since 4 + 1 + 7 = 12 and 3 + 9 = 12, the returned index would be 3
+
+ints = [1, 0, -1]
+# Returns None/nil/undefined/etc (depending on the language) as there
+# are no indices where the left and right sums are equal
+
+Start: 6:20am
+End: 06:50am
+Cycles: 1
+"""
+
+
+
+
