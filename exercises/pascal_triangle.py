4a6a3b6aecb6e2f7e12e2c0dc2c396138ffe0157
jasonmpittman <jason@jasonmpittman.com>
2025-11-11 07:07:02 -0500

    initial commit

---
+++ exercises/pascal_triangle.py
@@ -0,0 +1,23 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Write a function that, given a depth n, returns n top rows of Pascal's Triangle flattened into a one-dimensional list/array.
+Example:
+
+n = 1: [1]
+n = 2: [1,  1, 1]
+n = 4: [1,  1, 1,  1, 2, 1,  1, 3, 3, 1]
+
+Start: 7:05am
+End: 07:35am
+Cycles: 1
+"""
+
+
