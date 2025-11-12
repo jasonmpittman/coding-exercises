782260d422d2237ce66fb161c2ba2004ac7279ba
jasonmpittman <jason@jasonmpittman.com>
2025-11-12 04:36:17 -0500

    initial commit

---
+++ exercises/check_tree_balance.py
@@ -0,0 +1,16 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Write a function that checks if a BST is balanced. The tree is balanced if for each vertex, the size of the left subtree differs from the size of the right subtree by at most 1.
+
+Start: 4:35am
+End: 04:55am
+Cycles: 1
+"""
