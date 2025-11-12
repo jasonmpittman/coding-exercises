38e00046e0a4766ca93034d072fcace683f04ae7
jasonmpittman <jason@jasonmpittman.com>
2025-11-12 04:42:25 -0500

    initial commit

---
+++ exercises/len_and_count.py
@@ -0,0 +1,26 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Implement length to count the number of nodes in a linked list.
+
+    length(null) => 0
+    length(1 -> 2 -> 3 -> null) => 3
+
+Implement Count() to count the occurrences of an integer in a linked list.
+
+    count(null, 1) => 0
+    count(1 -> 2 -> 3 -> null, 1) => 1
+    count(1 -> 1 -> 1 -> 2 -> 2 -> 2 -> 2 -> 3 -> 3 -> null, 2) => 4
+
+
+Start: 4:45am
+End: 05:15am
+Cycles: 1
+"""
