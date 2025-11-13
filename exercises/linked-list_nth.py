0b9717395232ca849345d224b341265872d08dc0
jasonmpittman <jason@jasonmpittman.com>
2025-11-13 06:08:19 -0500

    initial commit

---
+++ exercises/linked-list_nth.py
@@ -0,0 +1,26 @@
+__author__ = "Jason M. Pittman"
+__copyright__ = "Copyright 2025"
+__credits__ = ["Jason M. Pittman"]
+__license__ = "MIT License"
+__version__ = "1.0.0"
+__maintainer__ = "Jason M. Pittman"
+__status__ = "Exercise"
+__difficuluty__ = ""
+
+"""
+Implement a GetNth() function that takes a linked list and an integer index and returns the node stored at the Nth index position. GetNth() uses the C numbering convention that the first node is index 0, the second is index 1, ... and so on.
+
+So for the list 42 -> 13 -> 666, GetNth(1) should return Node(13);
+
+getNth(1 -> 2 -> 3 -> null, 0).data === 1
+getNth(1 -> 2 -> 3 -> null, 1).data === 2
+
+
+Start: 6:10am
+End: 06:40am
+Cycles: 1
+"""
+
+
+
+
